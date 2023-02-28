8a82dec9a9e34f67fbd61533f0e68c3223b92824
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-02-28 21:44:36 +0200

    Finished chapter 1

---
+++ 0.IntroToProgramming/Book1/Chapter1/chapter1.js
@@ -0,0 +1,11 @@
+// Basic mathematical addition
+console.log(3 + 4);
+
+// Draw as many cats as you want!
+var drawCats = function (howManyTimes) {
+    for (var i = 0; i < howManyTimes; i++) {
+        console.log(i + " =^.^=");
+    }
+};
+
+drawCats(10); // You can put any number here instead of 10.
