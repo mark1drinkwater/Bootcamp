0a05941ffb83783bfc299cdc72dc91448c004265
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-03-01 11:31:53 +0200

    Updadted chapter 1 book 1

--- 0.IntroToProgramming/Book1/Chapter2/chapter2.js
+++ 0.IntroToProgramming/Book1/Chapter2/chapter2.js
@@ -37,5 +37,91 @@ age = 13;
 console.log(age);
 
 // JavaScript is case-sensitive!
+// numberofcandles is NOT the same as numberOfCandles
 
+// Creating new variables using math
+var secondsInAMinute = 60;
+var minutesInAnHour = 60;
+var secondsInAnHour = secondsInAMinute * minutesInAnHour;
+console.log('\n Seconds in an hour: ' + secondsInAnHour);
+
+var hoursInADay = 24;
+var secondsInADay = secondsInAnHour * hoursInADay;
+console.log('\n Seconds in a day: ' + secondsInADay);
+
+var daysInAYear = 365;
+var secondsInAYear = secondsInADay * daysInAYear;
+console.log('\n Seconds in a year: ' + secondsInAYear);
+
+var age = 29;
+age * secondsInAYear;
+console.log('\n Age: ' + age + ' in seconds');
+
+// Incrementing & Decrementing
+var highFives = 0;
+console.log('\nIncrement Before:');
+++highFives;
+console.log('You have: ' + highFives);
+++highFives;
+console.log('You have: ' + highFives);
+++highFives;
+console.log('You have: ' + highFives);
+
+console.log('\nIncrement After:')
+highFives++;
+console.log('You have: ' + highFives);
+highFives++;
+console.log('You have: ' + highFives);
+highFives++;
+console.log('You have: ' + highFives);
+
+// Plus-equals and Minus-equals
+var x = 10;
+x = x + 5;
+x;
+// Shorter way
+x += 5;
+
+//Minus-equals
+x -= 20;
+
+// ------------------------- Strings ------------------------
+var myAwesomeString = "Something REALLY awesome!!!";
+console.log('\nStrings:\n' + myAwesomeString);
+
+// You can re-assign variables e.g.
+ //A integer gets re-assigned to become a string
+var myThing = 5;
+myThing = "this is a string";
+
+// A number is a string if it's inside quotes
+var numberNine = 9;
+var stringNine = "9";
+
+// Add a number and a number
+console.log('\n Add a number and a number' + (numberNine + numberNine) );
+// Add a string and a string
+console.log('\n Add a string and a string' + (stringNine + stringNine) );
+
+// Add strings
+var greeting = "Hello ";
+var myName = "Nick";
+console.log('\nAdd strings: ' + greeting + myName);
+
+// --- Find the length of strings ---
+console.log("Length of strings: " + "Supercalifragilisticexpialidocious".length);
+
+// --- Getting a single character from a string ---
+var myName = "Nick";
+console.log('\nGetting a single character from a string')
+console.log(myName[0]);
+console.log(myName[1]);
+console.log(myName[2]);
+
+// Secret code
+var codeWord1 = "are";
+var codeWord2 = "tubas";
+var codeWord3 = "unsafe";
+var codeWord4 = "?!";
+console.log('\n Secret code word' + codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1]);
 
