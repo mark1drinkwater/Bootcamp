b68d96e3e68f36567986148b4fbbb7e9dff3fcd4
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-03-01 10:17:35 +0200

    Added chapter 2 for book 1

---
+++ 0.IntroToProgramming/Book1/Chapter2/chapter2.js
@@ -0,0 +1,41 @@
+// Topic: Data Types
+
+// Number: 5;
+
+// String: "Hi, I'm a string";
+
+//Boolean: true; false;
+
+// ---------- Numbers and Operators ----------
+
+// Addition
+console.log('Addition');
+console.log(12345 + 56789);
+
+// Subtraction
+console.log('\nSubtraction');
+console.log(1000 - 17);
+
+// Multiplication
+console.log('\nMultiplication');
+console.log(123 * 456);
+
+// Division
+console.log('\nDivision');
+console.log(12345 / 250);
+
+// BODMAS -- Order of operations
+console.log('\nOrder of operations');
+console.log(1234 + 57 * 3 - 31 / 4);
+
+// ---------- Variables ----------
+console.log('\nVariables');
+var age = 12;
+console.log(age);
+console.log('\nAge variable changes from 12 to 13');
+age = 13;
+console.log(age);
+
+// JavaScript is case-sensitive!
+
+
