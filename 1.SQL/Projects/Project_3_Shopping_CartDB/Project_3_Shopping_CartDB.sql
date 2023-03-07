77d2eab93ada70db63811f2a6ddf6f7a2ae95953
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-03-07 20:34:11 +0200

    Added functions
    Added addProductToCartByName, checkoutCart functions

--- 1.SQL/Projects/Project_3_Shopping_CartDB/Project_3_Shopping_CartDB.sql
+++ 1.SQL/Projects/Project_3_Shopping_CartDB/Project_3_Shopping_CartDB.sql
@@ -5,6 +5,7 @@ DROP TABLE IF EXISTS products;
 DROP TABLE IF EXISTS users;
 
 ----------------- Create & Insert tables -------------------
+----- Products table -----
 CREATE TABLE products (
 	id bigserial PRIMARY KEY,
 	name varchar(50) NOT NULL,
@@ -13,9 +14,10 @@ CREATE TABLE products (
 
 INSERT INTO products (name, price)
 VALUES
-('Coke', 10),
-('Chips', 5);
+('coke', 10),
+('chips', 5);
 
+----- Users table ------
 CREATE TABLE users (
 	id bigserial PRIMARY KEY,
 	name varchar(30) NOT NULL
@@ -26,24 +28,27 @@ VALUES
 ('Arnold'),
 ('Sheryl');
 
+----- Cart table -----
 CREATE TABLE cart (
 	product_id bigint PRIMARY KEY,
 	qty bigint
 );
 
+----- Order header table -----
 CREATE TABLE order_header (
 	order_id bigserial PRIMARY KEY,
 	user_id bigint REFERENCES users(id) ON DELETE CASCADE,
 	order_date timestamp NOT NULL
 );
 
+----- Order details table ------
 CREATE TABLE order_details (
 	order_header bigint REFERENCES order_header(order_id) ON DELETE CASCADE,
 	prod_id bigint REFERENCES products(id),
 	qty bigint
 );
 
--------------- Add to cart function ----------------
+------------------------ Add to cart function --------------------------
 CREATE OR REPLACE FUNCTION addProductToCart(prod_id bigint)
 RETURNS void AS $$
 BEGIN
@@ -56,6 +61,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-------------------- Calling the function --------------------
 -- Add coke to the cart
 SELECT addProductToCart(1);
 -- Add coke to the cart
@@ -63,9 +69,10 @@ SELECT addProductToCart(1);
 -- Add chips to the cart
 SELECT addProductToCart(2);
 
+----- Result -----
 SELECT * FROM cart;
 
--------------------- Checkout -----------------------
+------------------------- Checkout ----------------------------
 INSERT INTO order_header (user_id, order_date)
 VALUES
 (2, '2023-04-15 15:30:00');
@@ -77,14 +84,15 @@ VALUES
 
 SELECT * FROM order_details;
 
----------------- Add multiple products to the products table ------------------
+---------------- Insert additional products to the products table ------------------
 INSERT INTO products (name, price)
 VALUES
-('flour', 43.99),
-('jellytots', 19.99),
-('oreo', 16.99),
+('chicken pie', 29.99),
+('jelly tots', 19.99),
+('oreos', 16.99),
 ('milk', 31.99);
 
+----------------------------------- STEP 3 -----------------------------------
 ---------------- Add multiple products to the cart ---------------------------
 SELECT * FROM cart;
 SELECT addProductToCart(4);
@@ -110,7 +118,7 @@ SELECT * FROM cart;
 -------------------------- Customer #1 Checkout -------------------------------
 INSERT INTO order_header (user_id, order_date)
 VALUES
-(1, '2023-04-15 15:30:00');
+(1, '2023-04-15 19:45:51');
 
 INSERT INTO order_details (order_header, prod_id, qty)
 VALUES
@@ -126,11 +134,6 @@ SELECT addProductToCart(1);
 SELECT addProductToCart(2);
 SELECT addProductToCart(3);
 SELECT addProductToCart(5);
-
--- TODO:
--- SELECT createOrder();
--- Takes all the orders for the cart adds to the order details
--- Clears cart
 
 INSERT INTO order_header (user_id, order_date)
 VALUES
@@ -148,15 +151,105 @@ DELETE FROM cart;
 ----------- Print the orders with select statements with inner joins --------
 ----------- Print a single order --------------------------------------------
 -- Show the user, and their order names, price, sum of all orders
-SELECT * 
-FROM order_header
-INNER JOIN order_details
-ON order_header.order_id = order_details.order_header
+SELECT users.name, oh.order_date, prod.name, prod.price, od.qty
+FROM order_header oh
+INNER JOIN order_details od
+ON oh.order_id = od.order_header
+INNER JOIN users
+ON users.id = oh.user_id
+INNER JOIN products prod
+ON prod.id = od.prod_id
 WHERE order_id = 3;
 
----------- Printing all orders for a days shopping -------------------------
-SELECT *
-FROM order_header
-INNER JOIN order_details
-ON order_header.order_id = order_details.order_header
+---------- Printing all orders for a day's shopping -------------------------
+SELECT users.name, oh.order_date, prod.name, prod.price, od.qty
+FROM order_header oh
+INNER JOIN order_details od
+ON oh.order_id = od.order_header
+INNER JOIN users
+ON users.id = oh.user_id
+INNER JOIN products prod
+ON prod.id = od.prod_id
 WHERE date_part('day', order_date) = '15';
+
+------------------------------------------------------------------------------
+------------------------------- Extra functions ------------------------------
+---------- Add a product to a cart by using the product's name ---------------
+CREATE OR REPLACE FUNCTION addProductToCartByName(product_name varchar)
+RETURNS void AS $$
+DECLARE
+	prod_id bigint;
+BEGIN	
+	prod_id := (SELECT id FROM products WHERE name = product_name);
+	
+	IF EXISTS (SELECT * FROM cart  WHERE product_id = prod_id)
+		THEN
+			UPDATE cart SET qty = qty + 1  WHERE product_id = prod_id;
+		ELSE
+			INSERT INTO cart (product_id,qty) VALUES (prod_id,1);
+		END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM cart;
+SELECT addProductToCartByName('coke');
+SELECT * FROM cart;
+
+-------------------------------------------------------------------
+------------ Checkout entire cart in a single function ------------
+CREATE OR REPLACE FUNCTION checkoutCart(user_name varchar)
+RETURNS void AS $$
+BEGIN
+	--- If user doesn't exist then add them to the table
+	IF NOT EXISTS (SELECT id FROM users WHERE name = user_name)
+		THEN
+			INSERT INTO users (name) VALUES (user_name);
+		END IF;
+
+	--- Insert into order_header
+	INSERT INTO order_header (user_id, order_date) 
+	VALUES 
+		(
+			(SELECT id FROM users WHERE name = user_name),
+			(SELECT to_timestamp( current_timestamp::varchar, 'YYYY-MM-DD HH24:MI:SS')::timestamp without time zone)
+		);
+		
+	--- Insert into order_details
+	INSERT INTO order_details
+	(
+	 	SELECT 
+			(SELECT order_id FROM order_header ORDER BY order_id DESC LIMIT 1),
+			product_id,
+			qty
+	 	FROM cart
+	);
+	
+	--- Clear the cart
+	DELETE FROM cart;
+END;
+$$ LANGUAGE plpgsql;
+
+------------------------------------------------------------------------------------------------
+--------------------------- Demonstration of checkoutCart() function ---------------------------
+-- Clear the cart before begining --
+DELETE FROM cart;
+
+-- Add products to the cart --
+SELECT addProductToCartByName('chicken pie');
+SELECT addProductToCartByName('chips');
+SELECT addProductToCartByName('chips');
+SELECT addProductToCartByName('coke');
+
+------ Check the cart out -----
+SELECT checkoutCart('Mark');
+
+----------- Display results of user's checked out cart -----------
+SELECT users.name, oh.order_date, prod.name, prod.price, od.qty
+FROM order_header oh
+INNER JOIN order_details od
+ON oh.order_id = od.order_header
+INNER JOIN users
+ON users.id = oh.user_id
+INNER JOIN products prod
+ON prod.id = od.prod_id
+WHERE users.name = 'Mark';
