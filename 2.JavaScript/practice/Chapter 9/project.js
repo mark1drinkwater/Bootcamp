f3c5a6aecda8efa22422bd666b66763f699c6cf2
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-04-14 16:53:01 +0200

    Changed title

--- 2.JavaScript/practice/Chapter 9/project.js
+++ 2.JavaScript/practice/Chapter 9/project.js
@@ -1,14 +1,13 @@
 // Chapter 9 Project
-// No 1
 const output = document.querySelector('.output');
-// No 2
 const mainList = document.querySelector("ul");
 mainList.id = "mainList";
-// No 3
+
 const divTags = document.querySelectorAll("div");
 console.log(divTags);
-// No 4
+
 for (let i = 0; i < divTags.length; i++) {
+    console.log(divTags[i].tagName);
     divTags[i].id = "id" + (i+1);
     if (i % 2) {
         divTags[i].style.color = "red";
