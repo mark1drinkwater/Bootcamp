a6c38887ea9660e81147aa31de876c28d448bc81
Mark Drinkwater <mark1drinkwater@gmail.com>
2023-06-01 17:03:30 +0200

    added data logging to debug error

--- 3.NodeJS/textbook_two/chapter_13/controllers/home.js
+++ 3.NodeJS/textbook_two/chapter_13/controllers/home.js
@@ -4,6 +4,8 @@ module.exports = async (req, res) => {
     // populate automatically references the specified document with the userid in the collection
     const blogposts = await BlogPost.find({}).populate('userid')
     console.log(req.session)
+    console.log('*************************************')
+    console.log(blogposts)
     res.render('index', {
         blogposts
     });
