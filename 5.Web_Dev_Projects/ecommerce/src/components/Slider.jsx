aee7f6d4561f3f1ca3ae19c059cf5aa963d31c9a
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-06-26 13:31:45 +0200

    start of ecommerce project

---
+++ 5.Web_Dev_Projects/ecommerce/src/components/Slider.jsx
@@ -0,0 +1,128 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons'
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    position: relative; 
+    overflow: hidden;
+`
+
+const Wrapper = styled.div`
+    height: 100%;
+    display: flex;
+    transform:translateX(-100vw);
+`
+
+const Arrow = styled.div`
+ width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+`
+
+const Slide = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    background-color: #${props=>props.bg};
+`;
+
+const ImgContainer = styled.div`
+    height: 100%;
+    flex: 1;
+`;
+
+const Image = styled.img`
+    height: 80%;
+`
+
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 50px;
+`;
+
+const Title = styled.h1`
+    font-size: 70px;
+`;
+
+const Desc = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`;
+
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent; 
+    cursor: pointer;
+`;
+
+const Slider = () => {
+
+    const handleClick = (direction) => {
+        
+    }
+
+    return (
+        <Container>
+            <Arrow direction="left" onClick={()=>handleClick("left")}>
+                <ArrowLeftOutlined />
+            </Arrow>
+            <Wrapper>
+                <Slide bg="f5fafd">
+                    <ImgContainer>
+                        <Image src="https://cdn-montecasino.pressidium.com/wp-content/uploads/2020/06/Truworths-Desktop-1024x576.jpg" />
+                    </ImgContainer>
+                    <InfoContainer>
+                        <Title>SUMMER SALE</Title>
+                        <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVEALS.</Desc>
+                        <Button>SHOP NOW</Button>
+                    </InfoContainer>
+                </Slide>
+                <Slide bg="fcf1ed">
+                    <ImgContainer>
+                        <Image src="https://cdn-montecasino.pressidium.com/wp-content/uploads/2020/06/Truworths-Desktop-1024x576.jpg" />
+                    </ImgContainer>
+                    <InfoContainer>
+                        <Title>WINTER SALE</Title>
+                        <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVEALS.</Desc>
+                        <Button>SHOP NOW</Button>
+                    </InfoContainer>
+                </Slide>
+                <Slide bg="fbf0f4">
+                    <ImgContainer>
+                        <Image src="https://cdn-montecasino.pressidium.com/wp-content/uploads/2020/06/Truworths-Desktop-1024x576.jpg" />
+                    </ImgContainer>
+                    <InfoContainer>
+                        <Title>POPULAR SALE</Title>
+                        <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVEALS.</Desc>
+                        <Button>SHOP NOW</Button>
+                    </InfoContainer>
+                </Slide>
+
+            </Wrapper>
+            <Arrow direction="right" onClick={()=>handleClick("right")}>
+                <ArrowRightOutlined />
+            </Arrow>
+        </Container>
+    )
+}
+
+export default Slider
