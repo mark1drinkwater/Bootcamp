10db7d75812d9748c108d87a12100e34d9c975c4
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-06-26 19:20:12 +0200

    moved data to it's own json file

---
+++ 5.Web_Dev_Projects/ecommerce/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://cdn-montecasino.pressidium.com/wp-content/uploads/2020/06/Truworths-Desktop-1024x576.jpg",
+        title: "SUMMER SALE",
+        desc: "DON'T COMRPOMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/DG69bQ4/2.png",
+        title: "WINTER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVEALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://www.nicepng.com/png/full/8-88585_women-fashion-png-example-of-magazine-cover.png",
+        title: "POPULAR SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVEALS.",
+        bh: "fbf0f4"
+    }
+]
