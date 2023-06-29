9778266416c0141b051e7e65c9bb01556891da00
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-06-29 21:00:37 +0200

    Moves image folder to be under assets subfolder

--- 3.NodeJS/textbook_two/chapter_10/controllers/storePost.js
+++ 3.NodeJS/textbook_two/chapter_10/controllers/storePost.js
@@ -3,12 +3,12 @@ const path = require('path')
 
 module.exports = (req, res) => {
     let image = req.files.image;
-    image.mv(path.resolve(__dirname, '../public/img', image.name), async (error) => {
+    image.mv(path.resolve(__dirname, '../public/assets/img', image.name), async (error) => {
         await BlogPost.create({
             title: req.body.title,
             body: req.body.body,
             username: req.body.username,
-            image: '/img/' + image.name
+            image: '/assets/img/' + image.name
         })
         res.redirect('/')
     })
