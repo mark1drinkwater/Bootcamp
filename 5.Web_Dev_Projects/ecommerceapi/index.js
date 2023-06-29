1e1b4a26d7fef4dcd700f8df2493aa0fcecb5fb0
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-06-29 21:10:54 +0200

    moved mongo url to env file

--- 5.Web_Dev_Projects/ecommerceapi/index.js
+++ 5.Web_Dev_Projects/ecommerceapi/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://admin:razRJv5W8vtcfOM6@cluster0.1ztmqpn.mongodb.net/shop?retryWrites=true&w=majority")
+mongoose.connect("")
 .then(() => console.log("DBConnection Successful!"))
 .catch((error) => {
     console.log(error);
