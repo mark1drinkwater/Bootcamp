b711d07995f14dbb586aa9452a264fbef4a33404
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-07-04 10:47:33 +0200

    added verifyToken route

---
+++ 5.Web_Dev_Projects/ecommerceapi/routes/verifyToken.js
@@ -0,0 +1,10 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.token;
+    if (authHeader) {
+        jwt.verify(token, process.env.JWT_SEC);
+    } else {
+        return res.status(401).json("You are not authenticated!")
+    }
+};
