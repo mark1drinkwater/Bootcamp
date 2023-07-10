dbe30aeab8a817aa9b6baf7b124db27f4218d486
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-07-10 15:38:07 +0200

    added product route

--- 5.Web_Dev_Projects/ecommerceapi/index.js
+++ 5.Web_Dev_Projects/ecommerceapi/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
+const productRoute = require("./routes/product");
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ app.use(express.json())
 
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/users", userRoute);
+app.use("/api/v1/products", productRoute);
 
 app.listen(process.env.PORT || 5000, ()=> {
     console.log("Backend server is running!");
