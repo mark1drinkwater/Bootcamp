63bf74a5cff96b6c28124b1575aa63f418f9bfbc
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-07-11 10:26:56 +0200

    added backend routing

--- 5.Web_Dev_Projects/ecommerceapi/index.js
+++ 5.Web_Dev_Projects/ecommerceapi/index.js
@@ -4,7 +4,11 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
-const productRoute = require("./routes/product");
+const productRoute = require("./routes/order");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cartsRoute = require("./routes/cart");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -14,11 +18,15 @@ mongoose.connect(process.env.MONGO_URL)
     console.log(error);
 });
 
+app.use(cors());
 app.use(express.json())
 
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/products", productRoute);
+app.use("/api/v1/orders", orderRoute);
+app.use("/api/v1/carts", cartsRoute);
+app.use("/api/v1/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, ()=> {
     console.log("Backend server is running!");
