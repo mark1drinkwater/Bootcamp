139df17d6e5609acc6d6a74096d13123c8f4b701
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-07-11 10:26:26 +0200

    added routing to frontend

--- 5.Web_Dev_Projects/ecommerce/src/App.jsx
+++ 5.Web_Dev_Projects/ecommerce/src/App.jsx
@@ -1,13 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
 import Product from './pages/Product';
 import Register from './pages/Register';
 import Cart from './pages/Cart';
+import Pay from './pages/Pay';
+import Success from './pages/Success';
+import {Switch}  from 'react-router-dom';
 
 function App() {
   return (
-    <Home/>
+    // <Router>
+    //   <Switch>
+    //     <Route path="/pay">
+    //       <Pay />
+    //     </Route>
+    //     <Route path="/success">
+    //       <Success />
+    //     </Route>
+    //   </Switch>
+    // </Router>
+    <Pay/>
   );
 }
 
