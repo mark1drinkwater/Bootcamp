44018dc358d5f065c090bb7bcc009bf851557cef
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-07-12 12:24:49 +0200

    added main root and product routes

--- 5.Web_Dev_Projects/ecommerce/src/App.jsx
+++ 5.Web_Dev_Projects/ecommerce/src/App.jsx
@@ -1,5 +1,3 @@
-import { Route, Routes } from 'react-router-dom';
-import { Router } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
@@ -8,21 +6,31 @@ import Register from './pages/Register';
 import Cart from './pages/Cart';
 import Pay from './pages/Pay';
 import Success from './pages/Success';
-import {Switch}  from 'react-router-dom';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    // <Router>
-    //   <Switch>
-    //     <Route path="/pay">
-    //       <Pay />
-    //     </Route>
-    //     <Route path="/success">
-    //       <Success />
-    //     </Route>
-    //   </Switch>
-    // </Router>
-    <Pay/>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+
+        <Route path="/products/:category">
+          <ProductList/>
+        </Route>
+
+        <Route path="/products/:id">
+          <ProductList/>
+        </Route>
+
+      </Switch>
+    </Router>
   );
 }
 
