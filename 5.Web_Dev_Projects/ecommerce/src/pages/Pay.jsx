621d1ef13dd6e3952896217ab4c87fadb394d06a
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-07-12 12:29:10 +0200

    correct port number

--- 5.Web_Dev_Projects/ecommerce/src/pages/Pay.jsx
+++ 5.Web_Dev_Projects/ecommerce/src/pages/Pay.jsx
@@ -1,7 +1,7 @@
 import { useHistory } from 'react-router-dom';
 import StripeCheckout from 'react-stripe-checkout';
 import { useState, useEffect } from 'react';
-const axios = require('axios');
+import axios from "axios";
 
 const KEY = 'pk_test_51NSLruF7zG3QVgpa7jLFTGlohzu36c1yc5X2cIy5ioYTFTaKGMsSZ7dgLdiiugue2L9gvaEt78DxVb6gvdBe2PTU00ZuAwHKUl';
 
@@ -10,20 +10,23 @@ const Pay = () => {
     const history = useHistory();
 
     const onToken = (token) => {
-        console.log(token)
+        setStripeToken(token);
     }
 
     useEffect(() => {
+
         const makeRequest = async () => {
             try {
-                const res = await axios.post("http://localhost:5000/api/checkout/payment", {
-                    tokenId: stripeToken.id,
-                    amount: 2000,
-                })
+                const res = await axios.post("http://localhost:3000/api/v1/checkout/payment",
+                    {
+                        tokenId: stripeToken.id,
+                        amount: 2000,
+                    }
+                )
                 console.log(res.data);
                 history.push('/success');
             } catch (error) {
-                console.log(error);
+                console.log("Error has occured in making a request to checkout the payment", error);
             }
         };
         stripeToken && makeRequest();
