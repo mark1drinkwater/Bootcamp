e478eaae602398b266e5c1a49a39620067f5cd3c
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-07-14 10:28:57 +0200

    prevented response from being twice

--- 5.Web_Dev_Projects/ecommerceapi/routes/auth.js
+++ 5.Web_Dev_Projects/ecommerceapi/routes/auth.js
@@ -30,28 +30,33 @@ router.post("/login", async (req, res) => {
     try {
         const user = await User.findOne(
             {
-                username: req.body.username 
+                username: req.body.username
             }
-            );
-        !user && res.status(401).json("Wrong credentials!");
+        );
+        if (!user) {
+            res.status(401).json("Incorrect username login!");
+            return;
+        }
 
         const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
         const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
-        originalPassword !== req.body.password &&
-            res.status(401).json("Wrong credentials!");
+        if (originalPassword !== req.body.password) {
+            res.status(401).json("Incorrect password!");
+            return;
+        }
 
         const accessToken = jwt.sign({
             id: user._id,
-            isAdmin: user.isAdmin,            
-        }, 
-        process.env.JWT_SEC,
-        {expiresIn:"3d"}
+            isAdmin: user.isAdmin,
+        },
+            process.env.JWT_SEC,
+            { expiresIn: "3d" }
         );
 
-        const {password,  ...others} = user._doc;
+        const { password, ...others } = user._doc;
 
-        res.status(200).json({others, accessToken});
+        res.status(200).json({ others, accessToken });
     } catch (error) {
         res.status(500).json(error);
     }
