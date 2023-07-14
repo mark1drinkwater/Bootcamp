6594263d73a29df316d39148fa80f5f02dd791e4
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-07-14 10:27:56 +0200

    added successful payment page

--- 5.Web_Dev_Projects/ecommerce/src/pages/Success.jsx
+++ 5.Web_Dev_Projects/ecommerce/src/pages/Success.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
 
 const Success = () => {
+  const location = useLocation();
+  console.log(location);
+
   return (
-    <div>Success</div>
+    <div>
+      <h1>Thank you for shopping! Payment was Successful. 😃</h1>
+    </div>
   )
 }
 
