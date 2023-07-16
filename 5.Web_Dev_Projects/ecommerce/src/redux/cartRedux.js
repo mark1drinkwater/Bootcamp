740ce55b10129afb053e7947c5a42fa5d3eb0619
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-07-16 17:50:20 +0200

    start of clear cart reducer

--- 5.Web_Dev_Projects/ecommerce/src/redux/cartRedux.js
+++ 5.Web_Dev_Projects/ecommerce/src/redux/cartRedux.js
@@ -1,20 +1,28 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
-    name:"cart",
-    initialState:{
-        products:[],
-        quantity:0,
-        total:0,
+    name: "cart",
+    initialState: {
+        products: [],
+        quantity: 0,
+        shipping: 0,
+        total: 0,
     },
-    reducers:{
-        addProduct:(state,action)=>{
+    reducers: {
+        addProduct: (state, action) => {
             state.quantity += 1;
             state.products.push(action.payload);
             state.total += action.payload.price * action.payload.quantity;
+            state.shipping = state.total * 0.05;
         },
+        clearCart: (state) => {
+            state.quantity = 0;
+            state.products = [];
+            state.shipping = 0;
+            state.total = 0;
+        }
     },
 });
 
-export const {addProduct} = cartSlice.actions;
+export const { addProduct, clearCart } = cartSlice.actions;
 export default cartSlice.reducer;
