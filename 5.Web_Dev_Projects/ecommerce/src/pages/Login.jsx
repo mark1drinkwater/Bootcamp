d3a441096ca7e495b3634d0f1417a20fa227c1ba
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-07-16 11:53:24 +0200

    added register link

--- 5.Web_Dev_Projects/ecommerce/src/pages/Login.jsx
+++ 5.Web_Dev_Projects/ecommerce/src/pages/Login.jsx
@@ -88,8 +88,8 @@ const Login = () => {
           <Input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
           <Button onClick={handleClick} disabled={isFetching}>LOGIN</Button>
           {error && <Error>Something went wrong...</Error>}
-          <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
-          <Link>CREATE A NEW ACCOUNT</Link>
+          <Link>FORGOT YOUR PASSWORD?</Link>
+          <Link href="register">CREATE A NEW ACCOUNT</Link>
         </Form>
       </Wrapper>
     </Container>
