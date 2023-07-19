3da823f921663b613c5b71cec4ae150f827e7dec
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-07-19 11:11:10 +0200

    added stripe payment

--- 5.Web_Dev_Projects/ecommerceapi/routes/stripe.js
+++ 5.Web_Dev_Projects/ecommerceapi/routes/stripe.js
@@ -1,25 +1,36 @@
 const router = require("express").Router();
-const KEY = process.env.STRIPE_KEY
-const stripe = require("stripe")(KEY);
+const env = require("dotenv").config({ path: "./.env" });
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
 
-router.post("/payment", (req, res) => {
-  console.log("SK key", process.env.STRIPE_KEY)
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2022-08-01",
+});
 
-  stripe.charges.create(
-    {
-      source: req.body.tokenId,
+router.get("/config", (req, res) => {
+  res.status(200).json({
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
+});
+
+router.post("/create-payment-intent", async (req, res) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      currency: "EUR",
       amount: req.body.amount,
-      currency: "usd",
-    },
-    (stripeErr, stripeRes) => {
-      if (stripeErr) {
-        console.log("Error occured in stripe", stripeErr)
-        res.status(500).json(stripeErr);
-      } else {
-        res.status(200).json(stripeRes);
-      }
-    }
-  );
+      automatic_payment_methods: { enabled: true },
+    });
+
+    // Send publishable key and PaymentIntent details to client
+    res.status(200).json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (e) {
+    return res.status(400).send({
+      error: {
+        message: e.message,
+      },
+    });
+  }
 });
 
 module.exports = router;
