27f8f19c86cd1ed54018cf148cb37e126c6c3586
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-07-20 12:11:14 +0200

    return to prevent duplicate response

--- 5.Web_Dev_Projects/ecommerceapi/routes/order.js
+++ 5.Web_Dev_Projects/ecommerceapi/routes/order.js
@@ -4,14 +4,14 @@ const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = requir
 const Order = require("../models/Order")
 const router = require("express").Router();
 
-router.post("/", verifyToken, async (req, res) => {
+router.post("/", async (req, res) => {
     const newOrder = new Order(req.body);
   
     try {
       const savedOrder = await newOrder.save();
-      res.status(200).json(savedOrder);
+      return res.status(200).json(savedOrder);
     } catch (err) {
-      res.status(500).json(err);
+      return res.status(500).json(err);
     }
   });
 
