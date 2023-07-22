4ae8159595b42b53296d60f5fb7985d5bf560bc5
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-07-22 20:47:31 +0200

    added console log output to debug error

--- 5.Web_Dev_Projects/ecommerceapi/routes/verifyToken.js
+++ 5.Web_Dev_Projects/ecommerceapi/routes/verifyToken.js
@@ -5,11 +5,15 @@ const verifyToken = (req, res, next) => {
   if (authHeader) {
     const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.JWT_SEC, (err, user) => {
-      if (err) res.status(403).json("Token is not valid!");
+      if (err) {
+        console.log("Token is not valid")
+        res.status(403).json("Token is not valid!");
+      }
       req.user = user;
       next();
     });
   } else {
+    console.log("No auth header provider")
     return res.status(401).json("No auth header provided.");
   }
 };
