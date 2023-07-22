cb0b98bb06e498e97921318a40e0020914f61559
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-07-22 20:47:46 +0200

    round off to nearest 2 decimals

--- 5.Web_Dev_Projects/youtube-react-admin/src/components/featuredInfo/FeaturedInfo.jsx
+++ 5.Web_Dev_Projects/youtube-react-admin/src/components/featuredInfo/FeaturedInfo.jsx
@@ -27,7 +27,7 @@ export default function FeaturedInfo() {
       <div className="featuredItem">
         <span className="featuredTitle">Revenue</span>
         <div className="featuredMoneyContainer">
-          <span className="featuredMoney">R{income[1]?.total}</span>
+          <span className="featuredMoney">R{income[1]?.total.toFixed(2)}</span>
           <span className="featuredMoneyRate">
             {Math.floor(perc)}%{" "}
             {perc < 0 ? (
