0eddae66593facbe6512906eab709ed29419776b
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-07-24 19:01:47 +0200

    added update user reducers

--- 5.Web_Dev_Projects/youtube-react-admin/src/redux/userRedux.js
+++ 5.Web_Dev_Projects/youtube-react-admin/src/redux/userRedux.js
@@ -50,12 +50,31 @@ const userSlice = createSlice({
             state.isFetching = false;
             state.error = true;
         },
-    }
+        //UPDATE
+        updateUserStart: (state) => {
+            state.isFetching = true;
+            state.error = false;
+        },
+        updateUserSuccess: (state, action) => {
+            state.isFetching = false;
+            state.error = false;
+            state.users[state.users.findIndex(item => item._id === action.payload.id)] = action.payload.user;
+        },
+        updateUserFailure: (state) => {
+            state.isFetching = false;
+            state.error = true;
+        },
+
+
+        
+    },
+
 });
 
 export const {
     loginStart, loginSuccess, loginFailure,
     getUsersStart, getUsersSuccess, getUsersFailure,
-    deleteUserStart, deleteUserSuccess, deleteUserFailure
+    deleteUserStart, deleteUserSuccess, deleteUserFailure,
+    updateUserStart, updateUserSuccess, updateUserFailure
 } = userSlice.actions;
 export default userSlice.reducer;
