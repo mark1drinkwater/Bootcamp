8049e92c0be535d1d46d4229f9369b8aea3dc546
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-07-26 10:11:28 +0200

    added first and last name fields to user model

--- 5.Web_Dev_Projects/ecommerceapi/models/User.js
+++ 5.Web_Dev_Projects/ecommerceapi/models/User.js
@@ -4,6 +4,8 @@ const UserSchema = new mongoose.Schema(
     {
         username: { type: String, required: true, unique: true },
         email: { type: String, required: true, unique: true },
+        firstName: {type: String, required: true},
+        lastName: {type: String, required: true},
         password: { type: String, required: true, unique: false },
         isAdmin: {
             type: Boolean,
