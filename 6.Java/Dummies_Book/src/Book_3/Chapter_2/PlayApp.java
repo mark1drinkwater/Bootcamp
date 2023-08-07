25df2a274ced188a94cbf09ba8d0687973516fe3
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-07 07:10:31 +0200

    Create PlayApp.java

---
+++ 6.Java/Dummies_Book/src/Book_3/Chapter_2/PlayApp.java
@@ -0,0 +1,18 @@
+package Book_3.Chapter_2;
+
+public class PlayApp {
+
+    public static void main(String[] args) {
+        Player p1 = new Player();
+        p1.setHealth(10);
+        System.out.println("Player 1 health status: " + p1.getHealth());
+
+        Player p2 = new Player();
+        p2.setHealth(-12);
+        System.out.println("Player 2 health status: " + p2.getHealth());
+
+        Player p3 = new Player();
+        p3.setHealth(120);
+        System.out.println("Player 3 health status: " + p3.getHealth());
+    }
+}
