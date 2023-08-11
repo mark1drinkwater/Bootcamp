ba214e83bf4c96233d129f3ea23e2f837c8889ba
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-11 18:45:55 +0200

    Create PackageTest.java

---
+++ 6.Java/Dummies_Book/src/Book_3/Chapter_8/PackageTest.java
@@ -0,0 +1,14 @@
+package Book_3.Chapter_8;
+
+import com.gmail.markdrinkwater.util.Console;
+
+public class PackageTest
+{
+    public static void main(String[] args)
+    {
+        while (Console.askYorN("Keep going?"))
+        {
+            System.out.println("D'oh!");
+        }
+    }
+}
