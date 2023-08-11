9b6ba479a397a4325d4496526c6f9d8b22a71c29
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-11 18:46:03 +0200

    Create ArraysTest.java

---
+++ 6.Java/Dummies_Book/src/Book_4/Chapter_2/ArraysTest.java
@@ -0,0 +1,16 @@
+package Book_4.Chapter_2;
+
+import java.util.Arrays;
+
+public class ArraysTest {
+
+    public static void main(String[] args) {
+        int[] startValues = new int[100];
+        Arrays.fill(startValues, (int)(Math.random() * 100) + 1);
+
+        for (int i: startValues) {
+            System.out.println(i);
+        }
+    }
+
+}
