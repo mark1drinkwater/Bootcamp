3efe31f7062894b5a13ea4c3128e4d8b3edd24d5
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-12 20:06:31 +0200

    Create Exercise4_1.java

---
+++ 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter04/Exercise4_1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.markdrinkwater.d83527gc10.Chapter04;
+
+/**
+ *
+ * @author Mark
+ */
+public class Exercise4_1 {
+
+    /**
+     * @param args the command line arguments
+     */       
+    public static void main(String args[]) {
+        String custName = "Mary Smith";
+        String itemDesc = "Shirt";
+        String message; 
+        
+        message = custName + " wants to purchase a " + itemDesc;
+        
+        System.out.println(message);
+    }
+}
