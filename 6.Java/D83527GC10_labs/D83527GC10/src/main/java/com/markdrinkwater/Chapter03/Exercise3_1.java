e3df10f7fa498b119a95cb86a0e92ca381f3c892
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-12 20:06:28 +0200

    Create Exercise3_1.java

---
+++ 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter03/Exercise3_1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.markdrinkwater.d83527gc10.Chapter03;
+
+/**
+ *
+ * @author Mark
+ */
+public class Exercise3_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+    }
+}
