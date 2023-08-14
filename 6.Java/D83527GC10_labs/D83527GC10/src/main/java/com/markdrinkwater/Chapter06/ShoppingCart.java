e348e4b03e8edb88ede3e4068b9f6f5c43578594
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-14 10:39:07 +0200

    Create ShoppingCart.java

---
+++ 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter06/ShoppingCart.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.markdrinkwater.d83527gc10.Chapter06;
+
+/**
+ *
+ * @author Mark
+ */
+// Exercise 6.2
+public class ShoppingCart {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        Item item1 = new Item();
+        Item item2 = new Item();
+        
+        item1.descr = "Pizza";
+        item2.descr = "Ice-cream";
+        
+        System.out.println("Item 1 description: " + item1.descr);
+        System.out.println("Item 2 description: " + item2.descr);
+        
+        item2 = item1;
+        System.out.println("Item 2 description after re-assignment: " + item2.descr);
+        System.out.println(item2.descr);
+    }
+}
