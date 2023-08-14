dc2a7e82e21b9725e9525d953fd51c1cce15849b
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-14 10:39:12 +0200

    Create ShoppingCart.java

---
+++ 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter07/Exercise2/ShoppingCart.java
@@ -0,0 +1,25 @@
+
+package com.markdrinkwater.Chapter07.Exercise2;
+
+public class ShoppingCart {
+    public static void main (String[] args){
+        String custName = "Steve Smith";
+        String firstName;
+        int spaceIdx;
+        StringBuilder sb;
+       
+        spaceIdx = custName.indexOf(" ");
+        firstName = custName.substring(0, spaceIdx);
+        System.out.println(firstName);
+          
+	//Instantiate and initialize sb to firstName.
+        sb = new StringBuilder(firstName);
+        
+	// Put the full name back together, using StringBuilder append method.
+	//   You can just enter the String literal for the last name.
+	//   Print the full name.
+        sb.append(" Smith");
+        
+        
+    }
+}
