cd16ff0a54951c698cbc35f24ef0ce96b2b9510b
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-14 10:39:14 +0200

    Create ShoppingCart.java

---
+++ 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter07/Exercise3/ShoppingCart.java
@@ -0,0 +1,19 @@
+
+package com.markdrinkwater.Chapter07.Exercise3;
+
+public class ShoppingCart {
+    public static void main(String[] args){
+        int int1;
+        
+	//  Declare and initialize variables of type long, float, and char.
+        long long1 = 900000000000L;
+        float float1 = 15494.995f;
+        char char1 = 'E';
+        
+        // Print the long variable.
+        System.out.println("long1");
+	// Assign the long to the int and print the int variable.
+        int1 = (int) long1;
+        System.out.println(int1);        
+    }
+}
