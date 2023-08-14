7a9e214988ff62c70eb2fff13ebaa0e1df398b9e
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-14 10:39:02 +0200

    Create Exercise5_2.java

---
+++ 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter05/Exercise5_2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.markdrinkwater.d83527gc10.Chapter05;
+
+/**
+ *
+ * @author Mark
+ */
+public class Exercise5_2 {
+    public static void main(String args[]) {
+        
+        String custName = "Mary Smith";
+        String message;
+        
+        String names[] = {"Ferrari", "Piano", "Mahogany Table", "Leather Lounge Suit"};
+        
+        message = custName + " wants to purchase " + names.length + " items.";
+        System.out.println("Third element of names array: " + names[2]);
+        System.out.println(message);
+
+    }
+}
