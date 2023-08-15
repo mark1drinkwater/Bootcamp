eac82f185923a0d0c9a58c77b9fba4e6d2c4ffd1
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-15 08:02:08 +0200

    Update League.java

--- 6.Java/D83527GC10_labs/labs/08-Methods/Practices/Practice1/08-Methods_Practice1/src/soccer/League.java
+++ 6.Java/D83527GC10_labs/labs/08-Methods/Practices/Practice1/08-Methods_Practice1/src/soccer/League.java
@@ -17,7 +17,29 @@ public class League {
      */
     public static void main(String[] args) {
         
-        /* Practice 8-1. Start of code that builds teams */
+       Team[] theTeams = createTeams();
+       Game[] theGames = createGames(theTeams);
+        
+        Game currGame = theGames[0];
+        
+        Goal goal1 = new Goal();
+        goal1.thePlayer = currGame.homeTeam.playerArray[2];
+        goal1.theTeam = currGame.homeTeam;
+        goal1.theTime = 55;
+        Goal[] theGoals = {goal1};
+        currGame.goals = theGoals;
+        
+        System.out.println("Goal scored after " + 
+                currGame.goals[0].theTime + " mins by " +
+                currGame.goals[0].thePlayer.playerName + " of " +
+                currGame.goals[0].theTeam.teamName);
+  
+    }
+    
+    
+    /* Practice 8-1. Add new static method createTeams() here */
+    public static Team[] createTeams() {
+         /* Practice 8-1. Start of code that builds teams */
         // Create team1      
         Player player1 = new Player();
         player1.playerName = "George Eliot";
@@ -43,30 +65,17 @@ public class League {
         team2.playerArray[2].playerName = "Rafael Sabatini";
         
         /* Practice 8-1. End of code that builds teams */
-        
-        /* Practice 8-1. Delete next three lines */
-        Game currGame = new Game();
-        currGame.homeTeam = team1;
-        currGame.awayTeam = team2;
-        
-        Goal goal1 = new Goal();
-        goal1.thePlayer = currGame.homeTeam.playerArray[2];
-        goal1.theTeam = currGame.homeTeam;
-        goal1.theTime = 55;
-        Goal[] theGoals = {goal1};
-        currGame.goals = theGoals;
-        
-        System.out.println("Goal scored after " + 
-                currGame.goals[0].theTime + " mins by " +
-                currGame.goals[0].thePlayer.playerName + " of " +
-                currGame.goals[0].theTeam.teamName);
-  
+        Team[] theTeams = {team1, team2};
+        return theTeams;
     }
     
-    
-    /* Practice 8-1. Add new static method createTeams() here */
-    
-    
     /* Practice 8-1. Add new static method createGames() here */
+    public static Game[] createGames(Team[] theTeams) {
+        Game theGame = new Game();
+        theGame.homeTeam = theTeams[0];
+        theGame.awayTeam = theTeams[1];
+        Game[] theGames = {theGame};
+        return theGames;
+    }
     
 }
