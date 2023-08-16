9c288862004277bc7a1cdfa8e291113845a255ab
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-16 10:08:57 +0200

    Update Exercise3_1.java

--- 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter03/Exercise3_1.java
+++ 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter03/Exercise3_1.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
  */
-package com.markdrinkwater.d83527gc10.Chapter03;
+package com.markdrinkwater.Chapter03;
 
 /**
  *
@@ -15,5 +15,6 @@ public class Exercise3_1 {
      */
     public static void main(String args[]) {
         // TODO code application logic here
+        System.out.println("Hello world!");
     }
 }
