13d79013586a1a3a6e4301a5ab2488d78f0d620c
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-16 10:10:09 +0200

    Create ShoppingCart.java

---
+++ 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter09/Exercise2/ShoppingCart.java
@@ -0,0 +1,14 @@
+package com.markdrinkwater.Chapter09.Exercise2;
+
+
+public class ShoppingCart {
+
+    public static void main(String args[]) {
+        // Declare, instantiate, and initialize a Customer object
+        Customer customer1 = new Customer("Mary Smith", "555-44-3212");
+
+	// Print the customer object name
+        System.out.println(customer1.getName());
+        
+    }
+}
