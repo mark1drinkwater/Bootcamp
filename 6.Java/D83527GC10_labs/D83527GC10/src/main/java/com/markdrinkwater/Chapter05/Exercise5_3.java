d6befc0d2d6b5328617d832622020502939d199e
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-16 10:09:40 +0200

    Create Exercise5_3.java

---
+++ 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter05/Exercise5_3.java
@@ -0,0 +1,31 @@
+
+package com.markdrinkwater.Chapter05;
+
+public class Exercise5_3 {
+
+    public static void main(String[] args) {
+        // local variables
+        String custName = "Mary Smith";
+        String message;
+        double price = 21.99;
+        int quantity = 2;
+        double tax = 1.04;
+
+        String items[];
+        items = new String[4];
+        items[0] = "Shirt";
+        items[1] = "Belt";
+        items[2] = "Scarf";
+        items[3] = "Skirt";
+        
+        message = custName + " wants to purchase "+items.length+" items.";
+        System.out.println(message);
+        
+       // Iterate through and print out the items from the items array
+        System.out.println("Items purchased: ");
+       for (String item: items) {
+           System.out.print(item + " ");
+       }
+        
+    }    
+}
