9bad3fd6251b51db3739e1405e28bc9bfe9ec52d
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-16 10:10:02 +0200

    Create Customer.java

---
+++ 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter09/Exercise1/Customer.java
@@ -0,0 +1,21 @@
+package com.markdrinkwater.Chapter09.Exercise1;
+
+
+public class Customer {
+    private String name;
+    private String ssn;
+   
+    // Encapsulate this class.  Make ssn read only.
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getSsn() {
+        return ssn;
+    }
+    
+}
