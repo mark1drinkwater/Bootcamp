ca34cc1cb8b8d289288189a2e3244098cac20a0c
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-18 11:28:52 +0200

    Update Order.java

--- 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter10/Exercise2/Order.java
+++ 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter10/Exercise2/Order.java
@@ -28,19 +28,19 @@ public class Order {
     public void calcDiscount() {
          if (custType == NONPROFIT) {
             if (total > 900)
-                discount = 0.1;
+                discount = 10;
             else
-                discount = 0.08;
+                discount = 8;
          } else if (custType == PRIVATE) {
             if (total > 900)
-                discount = 0.07;
+                discount = 7;
             else
                 discount = 0;
          } else if (custType == CORP) {
              if (total > 500) 
-                 discount = 0.08;
+                 discount = 8;
              else
-                 discount = 0.05;
+                 discount = 5;
          }
     }
 }
