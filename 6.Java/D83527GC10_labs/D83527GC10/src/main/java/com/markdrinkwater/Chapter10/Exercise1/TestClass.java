3899b862c37b2ffbe31603fe1f39a1579fd65048
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-18 11:28:49 +0200

    Update TestClass.java

--- 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter10/Exercise1/TestClass.java
+++ 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter10/Exercise1/TestClass.java
@@ -6,14 +6,10 @@ public class TestClass {
 
     public static void main(String args[]) {
         int x = 4, y = 9;
-        if (y / x < 3) {
-            x += y;
-        } else {
-            x *= y;
-        }
-        System.out.println("After if stmt, x = " + x);
+
         
          // Use a ternary operator to perform the same logic as above.
          x = ((y / x) < 3) ? (x += y) : (x *= y);
+         System.out.println(x);
     }
 }
