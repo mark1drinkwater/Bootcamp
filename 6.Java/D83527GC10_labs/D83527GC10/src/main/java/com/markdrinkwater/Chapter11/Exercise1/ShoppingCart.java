c666550181bc279c47388c43a2f2676b394ca4b4
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-18 11:28:54 +0200

    Update ShoppingCart.java

--- 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter11/Exercise1/ShoppingCart.java
+++ 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter11/Exercise1/ShoppingCart.java
@@ -1,18 +1,23 @@
-package ex11_1_exercise;
+package com.markdrinkwater.Chapter11.Exercise1;
 
 // import statements here:
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 
 
 public class ShoppingCart {
     public static void main(String[] args){
 	// Declare a LocalDateTime object, orderDate
-
+        LocalDate orderDate;
         
 	// Initialize the orderDate to the current date and time. Print it.
-
+        orderDate = LocalDate.now();
+        System.out.println("Order date: " + orderDate);
 
 	// Format orderDate using ISO_LOCAL_DATE; Print it.
-
-
+        String fDate = orderDate.format(DateTimeFormatter.ISO_DATE);
+        System.out.println("Formatted order date: " + fDate);
     }
 }
