e6ed3600c4f23c1f44486b10ba2c084fdd0700ab
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-18 11:29:18 +0200

    Update ShoppingCart.java

--- 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter11/Exercise4/ShoppingCart.java
+++ 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter11/Exercise4/ShoppingCart.java
@@ -1,15 +1,24 @@
-package ex11_4_exercise;
+package com.markdrinkwater.Chapter11.Exercise4;
+
+import java.util.ArrayList;
 
 public class ShoppingCart {
     public static void main(String[] args){   
         // Declare, instantiate, and initialize an ArrayList of Strings.  Print and test your code.
-        
+        ArrayList<String> cart = new ArrayList();
+        cart.add("Watch");
+        cart.add("Car");
+        cart.add("Chocolates");
         
         // add (insert) another element at a specific index
-
+        cart.add(2, "RTX 4090");
 
 	// Check for the existence of a specific String element.  
         //   If it exists, remove it.
+        if (cart.contains("Car")) {
+            cart.remove("Car");
+        }
         
+        System.out.println(cart);
     }
 }
