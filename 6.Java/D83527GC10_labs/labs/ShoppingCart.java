6fc62e3c23c16d6ada42c985f29b91af39052f07
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-19 16:32:07 +0200

    Create ShoppingCart.java

---
+++ 6.Java/D83527GC10_labs/labs/ShoppingCart.java
@@ -0,0 +1,22 @@
+package com.markdrinkwater.d83527gc10.labs.03-ShoppingCart.Exercises.Exercise1.ex03_1_exercise;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+
+
+/**
+ *
+ * @author Mark
+ */
+public class ShoppingCart {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+    }
+}
