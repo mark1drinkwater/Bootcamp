0bfa48bd89bbfacfa36c8b04b78097917e306320
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-20 19:17:08 +0200

    Update Team.java

--- 6.Java/D83527GC10_labs/labs/09-EncapConstructors/Practices/Practice1/09-EncapConstructors_Practice1/src/soccer/Team.java
+++ 6.Java/D83527GC10_labs/labs/09-EncapConstructors/Practices/Practice1/09-EncapConstructors_Practice1/src/soccer/Team.java
@@ -11,8 +11,36 @@ package soccer;
  * @author Administrator
  */
 public class Team {
+
+    /**
+     * @return the teamName
+     */
+    public String getTeamName() {
+        return teamName;
+    }
+
+    /**
+     * @param teamName the teamName to set
+     */
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    /**
+     * @return the playerArray
+     */
+    public Player[] getPlayerArray() {
+        return playerArray;
+    }
+
+    /**
+     * @param playerArray the playerArray to set
+     */
+    public void setPlayerArray(Player[] playerArray) {
+        this.playerArray = playerArray;
+    }
     
-    public String teamName;
-    public Player[] playerArray;
+    private String teamName;
+    private Player[] playerArray;
     
 }
