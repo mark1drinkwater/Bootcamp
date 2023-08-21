a06bf016b5c1f4e80ac5c5bc6dfedeec4f8b91a0
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-21 11:58:09 +0200

    Update ShoppingCart.java

--- 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter12/Exercise1/ShoppingCart.java
+++ 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter12/Exercise1/ShoppingCart.java
@@ -1,10 +1,12 @@
-package ex12_1_exercise;
+package com.markdrinkwater.Chapter12.Exercise1;
 
 public class ShoppingCart {
     public static void main(String[] args){
 
 	// instantiate a Shirt object and call display() on the object reference
- 
+        Shirt myShirt = new Shirt(49.99, 'M', 'R');
+        myShirt.display();
+       
         
     }
 }
