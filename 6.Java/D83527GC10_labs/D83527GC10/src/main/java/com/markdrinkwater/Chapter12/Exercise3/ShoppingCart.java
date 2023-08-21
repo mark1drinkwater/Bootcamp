89e9e2b5d06a173c02465d02a418c4287571265b
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-21 11:58:34 +0200

    Update ShoppingCart.java

--- 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter12/Exercise3/ShoppingCart.java
+++ 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter12/Exercise3/ShoppingCart.java
@@ -1,11 +1,21 @@
-package ex12_3_exercise;
+package com.markdrinkwater.Chapter12.Exercise3;
 
 public class ShoppingCart {
     public static void main(String[] args) {
          // declare and instantiate a Shirt object using an Item reference type
-        
+        //Item myItem = new Shirt(49.99, 'M', 'R');
+        Item myItem = new Item("Tie", 19.99);
         
         // call the display method on the object, then the getColor method
-
+        myItem.display();
+        
+        if (myItem instanceof Shirt) {
+            String myColor = ((Shirt) myItem).getColor();           
+            
+            System.out.println("The shirt color is: " + myColor);
+        } else {
+            System.out.println("The item is not a shirt.");
+        }
+        
     }
 }
