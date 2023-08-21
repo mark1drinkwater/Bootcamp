3a905a4512249610ac5d06ace08730a08686f1c6
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-21 11:58:15 +0200

    Update Shirt.java

--- 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter12/Exercise2/Shirt.java
+++ 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter12/Exercise2/Shirt.java
@@ -1,4 +1,4 @@
-package ex12_2_exercise;
+package com.markdrinkwater.Chapter12.Exercise2;
 
 public class Shirt extends Item{
     private char size;
@@ -12,4 +12,38 @@ public class Shirt extends Item{
     
     // Override display() in the superclass to also show size and colorCode.
     //   Avoid duplicating code.
+    @Override
+    public void display(){
+        super.display();
+        System.out.println("\tSize: " + getSize() );
+        System.out.println("\tColor Code: " + getColorCode() );
+    }
+
+    /**
+     * @return the size
+     */
+    public char getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(char size) {
+        this.size = size;
+    }
+
+    /**
+     * @return the colorCode
+     */
+    public char getColorCode() {
+        return colorCode;
+    }
+
+    /**
+     * @param colorCode the colorCode to set
+     */
+    public void setColorCode(char colorCode) {
+        this.colorCode = colorCode;
+    }
 }
