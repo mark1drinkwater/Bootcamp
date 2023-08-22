ccd8bf6aa0afe3ac37b193a67507d2c2355461b2
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-22 08:00:42 +0200

    Update Team.java

--- 6.Java/D83527GC10_labs/labs/10-Conditions/Practices/Practice1/10-Conditions_Practice1/src/soccer/Team.java
+++ 6.Java/D83527GC10_labs/labs/10-Conditions/Practices/Practice1/10-Conditions_Practice1/src/soccer/Team.java
@@ -16,10 +16,26 @@ public class Team {
     private Player[] playerArray;
     
     /* Practice 10-1. Add pointTotal variable here */
+    private int pointsTotal;
     
+    /**
+     * @return the pointsTotal
+     */
+    public int getPointsTotal() {
+        return pointsTotal;
+    }
+
+    /**
+     * @param pointsTotal the pointsTotal to set
+     */
+    public void setPointsTotal(int pointsTotal) {
+        this.pointsTotal = pointsTotal;
+    }
     
     /* Practice 10-1. Add incPointsTotal() method here */
-    
+    public void incPointsTotal(int pointsTotal) {
+        this.pointsTotal += pointsTotal;       
+    }
     
     public Team(String teamName) {
         this.teamName = teamName;
