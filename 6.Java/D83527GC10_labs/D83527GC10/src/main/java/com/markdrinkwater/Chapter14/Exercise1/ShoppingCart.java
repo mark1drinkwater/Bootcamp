a58dcc0fcd538ce4f5c4f8aacea084dec567a321
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-23 11:22:50 +0200

    Create ShoppingCart.java

---
+++ 6.Java/D83527GC10_labs/D83527GC10/src/main/java/com/markdrinkwater/Chapter14/Exercise1/ShoppingCart.java
@@ -0,0 +1,19 @@
+package com.markdrinkwater.Chapter14.Exercise1;
+
+public class ShoppingCart {
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        int addResult = calc.add(43, 79);
+        System.out.println("Add Result: " + addResult);
+        
+        // Add exception handling to catch ArithmeticException
+        try {
+            double divResult = calc.divide(15, 0);
+            System.out.println("Division Result: " + divResult); 
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        }                       
+                
+    }
+}
