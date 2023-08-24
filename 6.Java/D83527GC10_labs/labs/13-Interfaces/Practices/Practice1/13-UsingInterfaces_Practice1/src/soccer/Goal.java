5eaa68654969896a59ba5faa422fb10560ffdb52
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-24 11:57:28 +0200

    Update Goal.java

--- 6.Java/D83527GC10_labs/labs/13-Interfaces/Practices/Practice1/13-UsingInterfaces_Practice1/src/soccer/Goal.java
+++ 6.Java/D83527GC10_labs/labs/13-Interfaces/Practices/Practice1/13-UsingInterfaces_Practice1/src/soccer/Goal.java
@@ -13,4 +13,7 @@ package soccer;
 public class Goal extends GameEvent {
     
     /* Practice 13-1. Add a toString() method */
+    public String toString() {
+        return "Goal scored";
+    }
 }
