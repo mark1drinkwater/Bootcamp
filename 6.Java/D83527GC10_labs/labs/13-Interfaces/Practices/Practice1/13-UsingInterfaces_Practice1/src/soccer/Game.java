3d4eab5254c21c4c92ec0614fe0a8a5fc34100c3
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-24 11:57:26 +0200

    Update Game.java

--- 6.Java/D83527GC10_labs/labs/13-Interfaces/Practices/Practice1/13-UsingInterfaces_Practice1/src/soccer/Game.java
+++ 6.Java/D83527GC10_labs/labs/13-Interfaces/Practices/Practice1/13-UsingInterfaces_Practice1/src/soccer/Game.java
@@ -33,8 +33,8 @@ public class Game {
         GameEvent currEvent;
         for (int i = 1; i <=90; i++){
             /* Practice 13-1. Change expression to Math.random > 0.8 */
-            if (Math.random() > 0.95){
-                currEvent = Math.random() > 0.6?new Goal():new Possession();
+            if (Math.random() > 0.8){
+                currEvent = Math.random() > 0.8?new Goal():new Possession();
                 /* Practice 13-1. Change first part of ternary operator toMath.random > 0.8 */
                 currEvent.setTheTeam(Math.random() > 0.5?homeTeam: awayTeam);
                 currEvent.setThePlayer(currEvent.getTheTeam().
@@ -62,12 +62,14 @@ public class Game {
         for (GameEvent currEvent: this.getEvents()) {
             
             /* Practice 13-1. Start if block here to determine if currEvent is a Goal */
-            if (currEvent.getTheTeam()== homeTeam) {
-                homeTeamGoals++;
-                homeTeam.incGoalsTotal(1);
-            } else {
-                awayTeamGoals++;
-                awayTeam.incGoalsTotal(1);
+            if (currEvent instanceof Goal) {
+                if (currEvent.getTheTeam()== homeTeam) {
+                    homeTeamGoals++;
+                    homeTeam.incGoalsTotal(1);
+                } else {
+                    awayTeamGoals++;
+                    awayTeam.incGoalsTotal(1);
+                }
             }
             /* Practice 13-1. End if block here to determine if currEvent is a Goal */
             
