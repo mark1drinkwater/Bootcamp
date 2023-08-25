40ca768f50cfb3ebfc116b19cd13240f58caee2b
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-25 12:09:55 +0200

    Update Player.java

--- 6.Java/D83527GC10_labs/labs/13-Interfaces/Practices/Practice3/13-UsingInterfaces_Practice3/src/soccer/Player.java
+++ 6.Java/D83527GC10_labs/labs/13-Interfaces/Practices/Practice3/13-UsingInterfaces_Practice3/src/soccer/Player.java
@@ -34,4 +34,24 @@ public class Player {
         this.playerName = playerName;
     }
     
+    private int goalsScored;
+
+    /**
+     * @return the goalsScored
+     */
+    public int getGoalsScored() {
+        return goalsScored;
+    }
+
+    /**
+     * @param goalsScored the goalsScored to set
+     */
+    public void setGoalsScored(int goalsScored) {
+        this.goalsScored = goalsScored;
+    }
+    
+    public void incGoalsScored() {
+        this.goalsScored++;
+    }
+    
 }
