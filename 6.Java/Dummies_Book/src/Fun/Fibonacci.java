51ac4e9fc5729cbdce1dab38e8d8eff2c825671a
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-27 10:31:41 +0200

    Create Fibonacci.java

---
+++ 6.Java/Dummies_Book/src/Fun/Fibonacci.java
@@ -0,0 +1,26 @@
+package Fun;
+
+public class Fibonacci {
+
+    public static void main (String[] args) {
+        System.out.println( fib(5) );
+//        System.out.println( fact(5) );
+    }
+
+    public static int fib(int n) {
+        if (n >= 3) {
+            return fib(n-1) + fib(n-2);
+        } else {
+            return 1;
+        }
+    }
+
+    public static int fact (int n) {
+        if (n >= 1) {
+            return n * fact(n-1);
+        } else {
+            return 1;
+        }
+    }
+
+}
