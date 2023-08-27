5c947dbe725ba5d6677f16cb5ad6391ee4ab6701
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-27 10:31:39 +0200

    Update ArraysTest.java

--- 6.Java/Dummies_Book/src/Book_4/Chapter_2/ArraysTest.java
+++ 6.Java/Dummies_Book/src/Book_4/Chapter_2/ArraysTest.java
@@ -1,10 +1,13 @@
 package Book_4.Chapter_2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ArraysTest {
 
     public static void main(String[] args) {
+        ArrayList<String> myArrayList = new ArrayList<String>();
+
         int[] startValues = new int[100];
         Arrays.fill(startValues, (int)(Math.random() * 100) + 1);
 
