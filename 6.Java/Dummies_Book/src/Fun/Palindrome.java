7aeb130b2233a164f01903fe01164c1fc5f6796c
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-27 11:17:01 +0200

    Prevented array starting out of bounds and changed for loop to incl. index 0

--- 6.Java/Dummies_Book/src/Fun/Palindrome.java
+++ 6.Java/Dummies_Book/src/Fun/Palindrome.java
@@ -5,13 +5,19 @@ public class Palindrome {
     public boolean isPalindrome(String word1, String word2) {
 
         String word1Reversed = "";
-        for (int i = word1.length(); i > 0; i--) {
+        for (int i = word1.length()-1; i >= 0; i--) {
             word1Reversed += word1.charAt(i);
         }
+
+        System.out.println(word1Reversed + " || " + word2);
 
         if (word1Reversed.equals(word2))
             return true;
         return false;
+    }
 
+    public static void main(String[] args) {
+        Palindrome pal1 = new Palindrome();
+        System.out.println(pal1.isPalindrome("mah", "ham"));
     }
 }
