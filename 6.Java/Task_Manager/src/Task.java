dc7e7a2e0de3e25c9a75dd4b28c0ce4ef9f6b953
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-28 11:27:48 +0200

    Added member fields to class

--- 6.Java/Task_Manager/src/Task.java
+++ 6.Java/Task_Manager/src/Task.java
@@ -1,2 +1,11 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Task {
+
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private Status status;
+
 }
