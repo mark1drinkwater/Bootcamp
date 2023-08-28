7e19e3a694b5a4c3fe9d34a28a3843e0d38716f9
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-28 11:48:48 +0200

    added getters & setters

--- 6.Java/Task_Manager/src/Task.java
+++ 6.Java/Task_Manager/src/Task.java
@@ -3,9 +3,50 @@ import java.time.LocalDateTime;
 
 public class Task {
 
+    private String taskId;
     private String title;
     private String description;
     private LocalDate dueDate;
     private Status status;
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 
 }
