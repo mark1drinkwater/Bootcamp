87129ef653c2a67dd8c1f367fcac37ba38cb2f6a
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-29 17:50:22 +0200

    Create Employee.java

---
+++ 6.Java/Dummies_Book/src/com/Fun/Employee.java
@@ -0,0 +1,22 @@
+package com.Fun;
+
+public class Employee
+{
+
+    public void changeMyName(String name) {
+        name = "Nuvalin";
+    }
+
+
+
+    public static void main (String[] args) {
+        String myName = "Mark";
+
+        System.out.println(myName);
+
+
+
+        System.out.println(myName);
+
+    }
+}
