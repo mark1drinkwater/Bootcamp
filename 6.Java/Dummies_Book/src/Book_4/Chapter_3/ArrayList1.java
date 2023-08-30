605a4579da1c43976479660e3ac8f25c43a7ac6c
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-08-30 18:35:03 +0200

    Update ArrayList1.java

--- 6.Java/Dummies_Book/src/Book_4/Chapter_3/ArrayList1.java
+++ 6.Java/Dummies_Book/src/Book_4/Chapter_3/ArrayList1.java
@@ -8,52 +8,53 @@ public class ArrayList1 implements Iterable {
 
     public static void main (String[] args) {
 
-//        ArrayList signs3;
-//
-//        ArrayList signs1 = new ArrayList();
-//        ArrayList signs2 = new ArrayList(100);
-//        signs2.add(88);
-//        signs2.add("hi");
-//        System.out.println(signs2);
+        ArrayList signs3;
 
-//        ArrayList<String> stringsOnly = new ArrayList<>();
-//        stringsOnly.add("Mark");
-//        Employee emp1 = new Employee("Mark", "Drinkwater");
-//        stringsOnly.add(emp1.toString());
-//
-//        System.out.println(stringsOnly);
+        ArrayList signs1 = new ArrayList();
+        ArrayList signs2 = new ArrayList(100);
 
-//        ArrayList<Employee> empList = new ArrayList<>();
+        signs2.add(88);
+        signs2.add("hi");
+        System.out.println(signs2);
+
+        ArrayList<String> stringsOnly = new ArrayList<>();
+        stringsOnly.add("Mark");
+        Employee emp1 = new Employee("Mark", "Drinkwater");
+        stringsOnly.add(emp1.toString());
+
+        System.out.println(stringsOnly);
+
+        ArrayList<Employee> empList = new ArrayList<>();
 //        Employee emp1 = new Employee("Mark", "Drinkwater");
-//        empList.add(emp1);
-//        System.out.println(empList);
+        empList.add(emp1);
+        System.out.println(empList);
 
         ArrayList<String> nums = new ArrayList<>(2);
         nums.add("One");
         nums.add("Two");
         nums.add("Three");
         nums.add("Four");
-//        nums.add("Five");
+        nums.add("Five");
 
-//        String first = nums.set(0, "Uno");
-//        String second = nums.set(1, "Dos");
-//        String third = nums.set(2, "Tres");
-//
-//        System.out.println(first);
-//        System.out.println(second);
-//        System.out.println(third);
-//        System.out.println(nums);
-//        System.out.println(nums.size());
+        String first = nums.set(0, "Uno");
+        String second = nums.set(1, "Dos");
+        String third = nums.set(2, "Tres");
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        System.out.println(nums);
+        System.out.println(nums.size());
 //        nums.add(10, "Ten");
 
-//        for (int i = 0; i < nums.size(); i++) {
-//            System.out.println( nums.get(i) );
-//        }
+        for (int i = 0; i < nums.size(); i++) {
+            System.out.println( nums.get(i) );
+        }
 
-//        for (String num: nums) {
-//            int i = nums.indexOf(num);
-//            System.out.println("Item " + i + ": " + num);
-//        }
+        for (String num: nums) {
+            int i = nums.indexOf(num);
+            System.out.println("Item " + i + ": " + num);
+        }
 
         Iterator e = nums.iterator();
         String s;
