5fe03962faa04bcb7e976e7061d6344ca71d2d0d
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-03 08:31:28 +0200

    Added comments explaining code.

--- 6.Java/Dummies_Book/src/Book_5/Chapter_1/CountDownV1/CountDownClock.java
+++ 6.Java/Dummies_Book/src/Book_5/Chapter_1/CountDownV1/CountDownClock.java
@@ -2,12 +2,16 @@ package Book_5.Chapter_1.CountDownV1;
 
 public class CountDownClock extends Thread
 {
+    // Override & implement the run method that comes from the Thread class that actually comes from the Runnable interface.
     @Override
     public void run() {
+        // Run timer for 20 seconds
         for (int t = 20; t > 0; t--)
         {
             System.out.println("T minus " + t);
+            // InterruptedException is a checked exception we must check for.
             try {
+                // Sleep every second
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 System.out.println(e.getMessage());
