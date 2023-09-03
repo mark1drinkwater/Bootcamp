285bb3014a5ae337698fc85b1c9a2784d06a189a
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-03 08:30:12 +0200

    Added comments explaining code.

--- 6.Java/Dummies_Book/src/Book_5/Chapter_1/CountDownV1/CountDownApp.java
+++ 6.Java/Dummies_Book/src/Book_5/Chapter_1/CountDownV1/CountDownApp.java
@@ -4,13 +4,16 @@ public class CountDownApp
 {
     public static void main(String[] args) {
         Thread clock = new CountDownClock();
+        // Interesting: You can store objects into type Runnable if they implement that interface.
         Runnable flood, ignition, liftoff;
 
+        // Storing the launch events into the runnable object.
         flood = new LaunchEvent(16, "Flood the pad!");
         ignition = new LaunchEvent(5, "Start the engines!");
         liftoff = new LaunchEvent(0, "Liftoff!");
         clock.start();
 
+        // Creating threads and passing the runnable objects to the constructor & then calling the start method.
         new Thread(flood).start();
         new Thread(ignition).start();
         new Thread(liftoff).start();
