778f9b61bb2e108c0eea0427fc25e1ec4585f760
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-04 11:16:52 +0200

    Create DirList.java

---
+++ 6.Java/Dummies_Book/src/Book_5/Chapter_3/DirList.java
@@ -0,0 +1,43 @@
+package Book_5.Chapter_3;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class DirList
+{
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Directory Lister.");
+
+        do {
+            System.out.print("\nEnter a path: ");
+            String path = sc.nextLine();
+
+            File dir = new File(path);
+
+            if (!dir.exists() || !dir.isDirectory())
+                System.out.println("That directory doesn't exist");
+        } while (askAgain());
+    }
+
+    private static void listDirectories(File dir, String indent) {
+        File[] dirs = dir.listFiles();
+        for (File f: dirs)
+        {
+            if (f.isDirectory())
+            {
+                System.out.println(indent + f.getName());
+                listDirectories(f, indent + " ");
+            }
+        }
+    }
+    private static boolean askAgain()
+    {
+        System.out.print("Another? (Y or N) ");
+        String reply = sc.nextLine();
+        if (reply.equalsIgnoreCase("Y"))
+            return true;
+        return false;
+    }
+}
