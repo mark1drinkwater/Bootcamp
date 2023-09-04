33f3b77e22956ddccf235aedfc610f35c9525584
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-04 08:39:54 +0200

    Added comments explaining CountDownV2 code

--- 6.Java/Dummies_Book/src/Book_5/Chapter_1/CountDownV2/CountDownAppV2.java
+++ 6.Java/Dummies_Book/src/Book_5/Chapter_1/CountDownV2/CountDownAppV2.java
@@ -5,16 +5,18 @@ import java.util.ArrayList;
 public class CountDownAppV2 {
     public static void main(String[] args) {
 
+        // Instantiate a CountDownClockV2 object.
         CountDownClockV2 clock = new CountDownClockV2(20);
-
-        ArrayList<Runnable> events =
-                new ArrayList<Runnable>();
-
+        // Instantiate a ArrayList that can store objects that implement the Runnable interface.
+        ArrayList<Runnable> events = new ArrayList<Runnable>();
+        // Create new LaunchEventV2 objects and add them to the event ArrayList
         events.add(new LaunchEventV2(16, "Flood the pad!", clock));
         events.add(new LaunchEventV2(6, "Start engines!", clock));
         events.add(new LaunchEventV2(0, "Liftoff!", clock));
+        // Start the clock
         clock.start();
-
+        // Loop through all the events, create a new thread passing it the object that implements the
+        //   runnable interface and then call the start method which calls the run method in turn.
         for (Runnable e : events) {
             new Thread(e).start();
         }
