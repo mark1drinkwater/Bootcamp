8970827a5111fa5c8319e7972f970a9c959bceb6
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-05 10:32:49 +0200

    Create ClickMe.java

---
+++ 6.Java/Java_Dummies_Book6/src/main/java/com/example/java_dummies_book6/ClickMe.java
@@ -0,0 +1,16 @@
+package com.example.java_dummies_book6;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ClickMe extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
