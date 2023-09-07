a19f443bf5d3c0ba52d088dc21e21b6213f9967b
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-07 10:09:09 +0200

    Create FlowPaneTest.java

---
+++ 6.Java/Java_Dummies_Book6/src/main/java/com/example/java_dummies_book6/Chapter_4/FlowPaneTest.java
@@ -0,0 +1,31 @@
+package com.example.java_dummies_book6.Chapter_4;
+
+import javafx.application.Application;
+import javafx.geometry.Orientation;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class FlowPaneTest extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn1 = new Button("Button One");
+        Button btn2 = new Button("Button Two");
+        Button btn3 = new Button("Button Three");
+        Button btn4 = new Button("Button Four");
+        Button btn5 = new Button("Button Five");
+        FlowPane pane = new FlowPane(Orientation.HORIZONTAL, 10, 10, btn1, btn2, btn3, btn4, btn5);
+        pane.setPrefWrapLength(300);
+
+        Scene scene = new Scene(pane,300, 400);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
