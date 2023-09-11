991723fd851bee4314ad798b1c099da1edc5d363
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-11 09:38:57 +0200

    Update module-info.java

--- 6.Java/Java_Dummies_Book6/src/main/java/module-info.java
+++ 6.Java/Java_Dummies_Book6/src/main/java/module-info.java
@@ -3,6 +3,16 @@ module com.example.java_dummies_book6 {
     requires javafx.fxml;
 
 
-    opens com.example.java_dummies_book6 to javafx.fxml;
-    exports com.example.java_dummies_book6;
+    exports com.example.java_dummies_book6.Chapter_1;
+    opens com.example.java_dummies_book6.Chapter_1 to javafx.fxml;
+    exports com.example.java_dummies_book6.Chapter_2;
+    opens com.example.java_dummies_book6.Chapter_2 to javafx.fxml;
+    exports com.example.java_dummies_book6.Chapter_3;
+    opens com.example.java_dummies_book6.Chapter_3 to javafx.fxml;
+    exports com.example.java_dummies_book6.Chapter_4;
+    opens com.example.java_dummies_book6.Chapter_4 to javafx.fxml;
+    exports com.example.java_dummies_book6.Chapter_5;
+    opens com.example.java_dummies_book6.Chapter_5 to javafx.fxml;
+    exports com.example.java_dummies_book6.Chapter_6;
+    opens com.example.java_dummies_book6.Chapter_6 to javafx.fxml;
 }
