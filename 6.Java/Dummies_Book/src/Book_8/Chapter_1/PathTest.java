9948336f9a9dc4415f632de0342d44b7805e8e4d
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-12 18:22:52 +0200

    Create PathTest.java

---
+++ 6.Java/Dummies_Book/src/Book_8/Chapter_1/PathTest.java
@@ -0,0 +1,28 @@
+package Book_8.Chapter_1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathTest
+{
+    public static void main(String[] args) {
+//        Path p = Paths.get("C:\\Users\\Mark\\Desktop\\Java\\TestArgs.java");
+//        if (Files.exists(p)) {
+//            System.out.println("The input file does exist!");
+//        } else {
+//            System.out.println("The input file does not exist!");
+//        }
+
+        Path p2 = Paths.get("C:\\Users\\Mark\\Desktop\\Java\\HelloWorld.java");
+        try {
+            Files.createFile(p2);
+            System.out.println("File created!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+    }
+}
