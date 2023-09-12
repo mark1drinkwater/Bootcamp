113841591e56af4b1e8bf034af42547d2eeb036e
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-12 18:22:40 +0200

    Create FileVisitorDemo.java

---
+++ 6.Java/Dummies_Book/src/Book_8/Chapter_1/FileVisitorDemo.java
@@ -0,0 +1,42 @@
+package Book_8.Chapter_1;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Objects;
+
+public class FileVisitorDemo
+{
+    public static void main(String[] args) {
+        Path start = Paths.get("c:\\Windows\\System32");
+        MyFileVisitor visitor = new MyFileVisitor();
+        try {
+            Files.walkFileTree(start, visitor);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static class MyFileVisitor extends SimpleFileVisitor<Path> {
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attr) {
+//            System.out.println(file.toString() + " ACCESSED");
+            return FileVisitResult.CONTINUE;
+        }
+
+
+        public FileVisitResult visitFileFailed(Path file, IOException e) {
+            System.out.println(file.toString() + " COULD NOT ACCESS!");
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult preVisitDirectory (Path dir, BasicFileAttributes attr) throws IOException {
+            if (!Files.isDirectory(dir))
+                System.out.println(dir.toString() + " COULD NOT ACCESS DIRECTORY");
+            return FileVisitResult.CONTINUE;
+        }
+
+    }
+}
