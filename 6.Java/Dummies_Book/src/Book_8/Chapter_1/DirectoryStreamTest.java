aa755971b79721bb561a30fadd47e63527666f9e
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-12 18:22:34 +0200

    Create DirectoryStreamTest.java

---
+++ 6.Java/Dummies_Book/src/Book_8/Chapter_1/DirectoryStreamTest.java
@@ -0,0 +1,25 @@
+package Book_8.Chapter_1;
+
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DirectoryStreamTest
+{
+    public static void main(String[] args) {
+        Path c = Paths.get("C:\\Users\\Mark\\Desktop\\Java");
+
+        try
+        {
+            DirectoryStream<Path> stream = Files.newDirectoryStream(c, "*.txt");
+
+            for (Path entry: stream)
+                System.out.println(entry.getFileName());
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
