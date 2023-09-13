311a012206f6592873aa06e79d0c3a09f71d7e8b
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-13 08:37:14 +0200

    New Myfile.java code

--- 6.Java/Dummies_Book/src/Book_8/Chapter_1/MyFile.java
+++ 6.Java/Dummies_Book/src/Book_8/Chapter_1/MyFile.java
@@ -7,40 +7,40 @@ public class MyFile {
 
     public static void main(String[] args) {
         File f =new File("C:\\Users\\Mark\\Documents\\Bootcamp\\6.Java\\helloToYou.txt");
-//        if (f.exists())
-//        {
-//            System.out.println("File does exist");
-//        } else {
-//            System.out.println("File does not exist");
-//        }
+        if (f.exists())
+        {
+            System.out.println("File does exist");
+        } else {
+            System.out.println("File does not exist");
+        }
 
-//        try {
-//            if (f.createNewFile()) {
-//                System.out.println("File created.");
-//            } else {
-//                System.out.println("File could not be created.");
-//            }
-//        } catch (IOException e) {
-//            System.out.println("Error creaking file.\nStack Trace:");
-//            e.printStackTrace(System.out);
-//        }
+        try {
+            if (f.createNewFile()) {
+                System.out.println("File created.");
+            } else {
+                System.out.println("File could not be created.");
+            }
+        } catch (IOException e) {
+            System.out.println("Error creating file.\nStack Trace:");
+            e.printStackTrace(System.out);
+        }
 
-//        File dir = new File("C:\\Users\\Mark\\Documents\\Bootcamp\\6.Java");
-//
-//        if (dir.isDirectory()) {
-//            System.out.println("Yes it's a directory.\nHere is it's file contents:\n");
-//            File[] files = dir.listFiles();
-//            for (File myFile: files) {
-//                System.out.println(myFile.getName());
-//            }
-//        }
+        File dir = new File("C:\\Users\\Mark\\Documents\\Bootcamp\\6.Java");
 
-//
-//        if (f.renameTo(new File("C:\\Users\\Mark\\Documents\\Bootcamp\\6.Java\\helloToYou.txt"))) {
-//            System.out.println("File renamed.");
-//        } else {
-//            System.out.println("File unable to rename.");
-//        }
+        if (dir.isDirectory()) {
+            System.out.println("Yes it's a directory.\nHere is it's file contents:\n");
+            File[] files = dir.listFiles();
+            for (File myFile: files) {
+                System.out.println(myFile.getName());
+            }
+        }
+
+
+        if (f.renameTo(new File("C:\\Users\\Mark\\Documents\\Bootcamp\\6.Java\\helloToYou.txt"))) {
+            System.out.println("File renamed.");
+        } else {
+            System.out.println("File unable to rename.");
+        }
 
         if (f.delete( )) {
             System.out.println("File deleted.");
