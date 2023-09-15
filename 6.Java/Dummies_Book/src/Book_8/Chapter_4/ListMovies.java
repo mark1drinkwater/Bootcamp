700b2180a737b1b8731a7eca81e0752af65f6eb0
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-15 09:07:01 +0200

    Create ListMovies.java

---
+++ 6.Java/Dummies_Book/src/Book_8/Chapter_4/ListMovies.java
@@ -0,0 +1,109 @@
+package Book_8.Chapter_4;
+
+import java.sql.*;
+import java.text.NumberFormat;
+
+public class ListMovies
+{
+    private static Connection con;
+
+    public static void main(String[] args) {
+        NumberFormat cf = NumberFormat.getCurrencyInstance();
+        ResultSet movies = getMovies();
+        try
+        {
+            while (movies.next())
+            {
+                Movie m = getMovie(movies);
+                String msg = Integer.toString(m.year);
+                msg += ": " + m.title;
+                msg += " (" + cf.format(m.price) + ")";
+                System.out.println(msg);
+            }
+
+            try
+            {
+                con.close();
+                System.out.println("Connection closed.");
+            } catch (Exception e) {
+                System.out.println("Error closing database.");
+            }
+
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static ResultSet getMovies()
+    {
+        con = getConnection();
+        try
+        {
+            Statement s = con.createStatement();
+            String select = "SELECT * FROM movie ORDER BY year";
+            ResultSet rows;
+            rows = s.executeQuery(select);
+            return rows;
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    private static Connection getConnection()
+    {
+        Connection con = null;
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost/Movies";
+            String user = "root";
+            String pw = "password";
+            con = DriverManager.getConnection(url, user, pw);
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+        return con;
+    }
+
+    private static Movie getMovie(ResultSet movies)
+    {
+        try
+        {
+            String title = movies.getString("Title");
+            int year = movies.getInt("Year");
+            double price = movies.getDouble("Price");
+            return new Movie(title, year, price);
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    private static class Movie
+    {
+        public String title;
+        public int year;
+        public double price;
+        public Movie(String title, int year, double price)
+        {
+            this.title = title;
+            this.year = year;
+            this.price = price;
+        }
+    }
+}
