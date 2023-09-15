a249b17fad4d1fdcf4e8469ce688afb255168853
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-15 09:06:59 +0200

    Create JDBCTest.java

---
+++ 6.Java/Dummies_Book/src/Book_8/Chapter_4/JDBCTest.java
@@ -0,0 +1,37 @@
+package Book_8.Chapter_4;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCTest {
+
+    public static void main(String[] args) {
+        Connection con = getconnection();
+    }
+
+    private static Connection getconnection()
+    {
+        Connection con = null;
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost/Movies";
+            String user = "root";
+            String pw = "password";
+            con = DriverManager.getConnection(url, user, pw);
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("SUCCESSFUL CONNECTION!");
+        return con;
+    }
+
+}
