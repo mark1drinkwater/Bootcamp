9833bf2891ce3e865b875f01ad74b276f5b6631a
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-16 10:24:47 +0200

    Create _6.java

---
+++ 6.Java/OCP_Java/src/Chapter1/_6.java
@@ -0,0 +1,11 @@
+package Chapter1;
+
+public class _6
+{
+    class Camel {
+        /*
+        Assuming the following class compiles, how many variables defined in the class or method
+        are in scope on the line marked on line 14?
+         */
+    }
+}
