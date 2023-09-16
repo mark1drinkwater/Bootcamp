ab26831b5abdfd3f4d56052dc67830f364a8861e
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-16 10:24:42 +0200

    Create _4.java

---
+++ 6.Java/OCP_Java/src/Chapter1/_4.java
@@ -0,0 +1,14 @@
+package Chapter1;
+
+public class _4
+{
+    // Which of the following are valid Java identifiers? (Choose all that apply.)
+    String _; // A
+    String _helloWorld$; // B
+    String true; // C
+    String java.lang; // D
+    String Public; // E
+    String 1980_s; // F
+
+    String _q2_; // G
+}
