0151c9a9883849a7b4b142f2da2488f9861ad2c4
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-17 23:00:09 +0200

    Create _7.java

---
+++ 6.Java/OCP_Java/src/Chapter1/_7.java
@@ -0,0 +1,12 @@
+package Chapter1;
+
+public class _7 {
+    private int numForks;
+    public static void main(String[] args) {
+        int numKnives;
+        System.out.print("""
+ "# forks = " + numForks +
+ " # knives = " + numKnives +
+ # cups = 0""");
+    }
+}
