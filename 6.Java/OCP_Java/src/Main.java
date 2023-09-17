7ac967cec87b57d47f2f8295bd14468ffb96c8dc
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-17 22:56:39 +0200

    Create Main.java

---
+++ 6.Java/OCP_Java/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        var pooh = """
+         "Oh, bother." -Pooh
+         """.indent(1);
+                System.out.print(pooh);
+    }
+}
