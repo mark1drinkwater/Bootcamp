ded91c9603ed3486c858df642e76da88c386193e
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-17 23:00:06 +0200

    Update _4.java

--- 6.Java/OCP_Java/src/Chapter1/_4.java
+++ 6.Java/OCP_Java/src/Chapter1/_4.java
@@ -3,12 +3,12 @@ package Chapter1;
 public class _4
 {
     // Which of the following are valid Java identifiers? (Choose all that apply.)
-    String _; // A
+//    String _; // A
     String _helloWorld$; // B
-    String true; // C
-    String java.lang; // D
+//    String true; // C
+//    String java.lang; // D
     String Public; // E
-    String 1980_s; // F
+//    String 1980_s; // F
 
     String _q2_; // G
 }
