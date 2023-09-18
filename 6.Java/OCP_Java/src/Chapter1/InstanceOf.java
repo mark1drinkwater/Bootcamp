ea18a7b38e7e4aed392121ba11a65771927f5ad0
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-18 08:23:29 +0200

    Comment out code to allow compilation

--- 6.Java/OCP_Java/src/Chapter1/InstanceOf.java
+++ 6.Java/OCP_Java/src/Chapter1/InstanceOf.java
@@ -4,15 +4,15 @@ import java.time.LocalDateTime;
 
 public class InstanceOf
 {
-    public static void openZoo(Number time) {
-        if(time instanceof String) // DOES NOT COMPILE
-            System.out.print(time);
-    }
+//    public static void openZoo(Number time) {
+//        if(time instanceof String) // DOES NOT COMPILE
+//            System.out.print(time);
+//    }
 
 
-    public static void main (String[] args) {
-        short shorty = 5;
-        openZoo(shorty );
-    }
+//    public static void main (String[] args) {
+//        short shorty = 5;
+//        openZoo(shorty );
+//    }
 
 }
