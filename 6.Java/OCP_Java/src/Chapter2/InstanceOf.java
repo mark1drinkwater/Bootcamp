fd90946b3434a3fe8ffc96d0e4883224457a097a
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-18 08:30:34 +0200

    Move files to chapter 2

--- 6.Java/OCP_Java/src/Chapter2/InstanceOf.java
+++ 6.Java/OCP_Java/src/Chapter2/InstanceOf.java
@@ -1,4 +1,4 @@
-package Chapter1;
+package Chapter2;
 
 import java.time.LocalDateTime;
 
--- 6.Java/OCP_Java/src/Chapter2/LogicalOperators.java
+++ 6.Java/OCP_Java/src/Chapter2/LogicalOperators.java
@@ -1,13 +1,15 @@
-package Chapter1;
+package Chapter2;
 
 public class LogicalOperators
 {
     public static void main(String[] args) {
         boolean eyesClosed = true;
         boolean breathingSlowly = true;
+
         boolean resting = eyesClosed | breathingSlowly;
         boolean asleep = eyesClosed & breathingSlowly;
         boolean awake = eyesClosed ^ breathingSlowly;
+
         System.out.println(resting); // true
         System.out.println(asleep); // true
         System.out.println(awake); // false
