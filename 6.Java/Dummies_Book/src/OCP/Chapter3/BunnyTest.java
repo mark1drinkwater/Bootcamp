3ac3dbfced5e33a7fd881f29b9edd4f84f8522fc
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-21 12:13:48 +0200

    Create BunnyTest.java

---
+++ 6.Java/Dummies_Book/src/OCP/Chapter3/BunnyTest.java
@@ -0,0 +1,15 @@
+package OCP.Chapter3;
+
+public class BunnyTest
+{
+    public static void main(String[] args) {
+        int count = 0;
+        BUNNY: for(int row = 1; row <=3; row++)
+            RABBIT: for(int col = 0; col <3 ; col++) {
+                if((col + row) % 2 == 0)
+                    break RABBIT;
+                count++;
+            }
+        System.out.println(count);
+    }
+}
