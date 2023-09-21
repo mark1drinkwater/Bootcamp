a7ed5926c2be4047b3d4e077333733e5e4e6abd8
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-21 12:13:50 +0200

    Create SnakeLoop.java

---
+++ 6.Java/Dummies_Book/src/OCP/Chapter3/SnakeLoop.java
@@ -0,0 +1,14 @@
+package OCP.Chapter3;
+
+public class SnakeLoop
+{
+//    public static void main(String[] args) {
+//        double iguana = 0;
+//        do {
+//            int snake = 1;
+//            System.out.print(snake++ + " ");
+//            iguana--;
+//            } while (snake <= 5);
+//         System.out.println(iguana);
+//    }
+}
