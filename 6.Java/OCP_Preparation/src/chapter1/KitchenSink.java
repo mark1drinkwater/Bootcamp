46bd337714a95a7c2b03dd7d3cc1ade5cf69d579
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-23 08:03:01 +0200

    Create KitchenSink.java

---
+++ 6.Java/OCP_Preparation/src/chapter1/KitchenSink.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chapter1;
+
+/**
+ *
+ * @author Mark
+ */
+public class KitchenSink {
+
+    private int numForks;
+
+    public static void main(String[] args) {
+        int numKnives;
+        System.out.print("""
+    "# forks = " + numForks +
+    " # knives = " + numKnives +
+    # cups = 0""");
+        
+        var day = 1/0;
+    }
+}
