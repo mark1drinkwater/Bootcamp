b61fa4a9232dec20855ffc20f5e63efdc68d68bb
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-23 08:02:53 +0200

    Create Animal.java

---
+++ 6.Java/OCP_Preparation/src/chapter1/Animal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chapter1;
+
+/**
+ *
+ * @author Mark
+ */
+public class Animal {
+
+    public static void main(String[] args) {
+        System.out.println("Bark!");
+    }    
+}
+
+class Animal2 {
+    public static void main(String[] args) {
+        System.out.println("Bleet!");
+    }
+}
