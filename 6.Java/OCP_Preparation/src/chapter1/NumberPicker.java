9943ebacec749c357d28e11c3f015406652de4b1
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-23 08:03:06 +0200

    Create NumberPicker.java

---
+++ 6.Java/OCP_Preparation/src/chapter1/NumberPicker.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chapter1;
+
+import java.util.*; //redundant import
+import java.util.Random;
+
+public class NumberPicker {
+
+    public static void main(String[] args) {
+        Random r = new Random();
+        System.out.println(r.nextInt(10));
+    }
+    
+}
