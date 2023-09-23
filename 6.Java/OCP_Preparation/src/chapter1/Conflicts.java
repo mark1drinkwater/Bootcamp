8938906f797f46636d63987f197b75da41306f93
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-23 08:02:58 +0200

    Create Conflicts.java

---
+++ 6.Java/OCP_Preparation/src/chapter1/Conflicts.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chapter1;
+
+import java.util.ArrayList;
+import java.util.*;
+import java.sql.Date;
+
+/**
+ *
+ * @author Mark
+ */
+public class Conflicts {
+
+    public static void main(String[] args) {
+        java.sql.Date sdate = new java.sql.Date(123, 05, 14);
+        java.util.Date udate = new java.util.Date(); 
+        
+        System.out.println(sdate);
+        System.out.println(udate.toString());    
+    }
+    
+}
