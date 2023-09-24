313670feaa106337fc683aadeb77b793a03c8671
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-24 07:48:19 +0200

    Create OrderOperations.java

---
+++ 6.Java/OCP_Preparation/src/chapter2/OrderOperations.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chapter2;
+
+/**
+ *
+ * @author Mark
+ */
+public class OrderOperations {
+
+    public static void main(String[] args) {
+        System.out.println( 15 * 5 / 3 + 5 );
+    
+    }
+    
+}
