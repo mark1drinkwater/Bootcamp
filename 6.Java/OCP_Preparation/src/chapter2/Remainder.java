69a83fc8a3717de813b6eb584105df7b68002684
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-24 07:48:03 +0200

    Create Remainder.java

---
+++ 6.Java/OCP_Preparation/src/chapter2/Remainder.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chapter2;
+
+/**
+ *
+ * @author Mark
+ */
+public class Remainder {
+
+    public static void main(String[] args) {
+        System.out.println(9 / 3); // 3
+        System.out.println(9 % 3); // 0
+        System.out.println(10 / 3); // 3
+        System.out.println(10 % 3); // 1
+        System.out.println(11 / 3); // 3
+        System.out.println(11 % 3); // 2
+        System.out.println(12 / 3); // 4
+        System.out.println(12 % 3); // 0
+    }
+    
+}
