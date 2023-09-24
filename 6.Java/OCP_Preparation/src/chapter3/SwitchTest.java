475f16f64d0acb7381ae446e5dcc7fee07d7d4b1
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-24 07:47:54 +0200

    Create SwitchTest.java

---
+++ 6.Java/OCP_Preparation/src/chapter3/SwitchTest.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chapter3;
+
+/**
+ *
+ * @author Mark
+ */
+public class SwitchTest {
+
+    enum Season {
+        WINTER, SPRING, SUMMER, FALL
+    }
+
+    public static void main(String[] args) {
+//        int fish = 5;
+//        int length = 12;
+//        var name = switch (fish) {
+//            case 1 ->
+//                "Goldfish";
+////            case 2 -> {};
+//            // All branches must return a value.
+//            case 3 -> {
+//                if (length > 10) {
+//                    yield "Blobfish";
+//                }
+//            }
+//            default ->
+//                "Swordfish";
+
+        getWeather(WINTER);
+    }
+    ;       
+        
+        String
+
+    static getWeather(Season value) {
+        return switch (value) {
+            case WINTER ->
+                "Cold";
+            case SPRING ->
+                "Rainy";
+            case SUMMER ->
+                "Hot";
+            case FALL ->
+                "Warm";
+        };
+    }
+
+}
