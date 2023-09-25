341d05283744b91a2ebac85d25f6612fd5c71c28
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-25 09:11:56 +0200

    Create VarTest.java

---
+++ 6.Java/OCP_Preparation/src/chapter1/VarTest.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chapter1;
+
+/**
+ *
+ * @author Mark
+ */
+public class VarTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        var doesCompile = "Yes";
+        //var doesNotCompile;
+        
+        //int a, var b = 3; // DOES NOT COMPILE
+        //var n = null; // DOES NOT COMPILE
+        
+        String msg = """
+                        Hello
+                            there
+                     """;
+    }
+    
+}
