a68174581ff6ef4db304cfff0ffce7dedb9922a5
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-25 09:12:59 +0200

    Create Widget.java

---
+++ 6.Java/IgBooks/src/igbook1/lesson6/interfaces/Widget.java
@@ -0,0 +1,27 @@
+package igbook1.lesson6.interfaces;
+
+public class Widget implements SalesCalcs {
+    double 
+    public Widget(double salesPrice, double cost, double weight) {
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public double calcSalesPrice() {
+        return 0;
+    }
+
+    @Override
+    public double calcCost() {
+        return 0;
+    }
+
+    @Override
+    public double calcProfit() {
+        return 0;
+    }
+}
