59061067324de7b1a0e92911470b62828a3d6470
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-09-27 07:37:36 +0200

    Create SetExample.java

---
+++ 6.Java/IgBooks/src/igbook1/lesson7/collections/SetExample.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package igbook1.lesson7.collections;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ *
+ * @author Mark
+ */
+public class SetExample {
+
+    public static void main(String[] args) {
+//        Set<String> set = new TreeSet<>();
+        Set<String> set = new HashSet<>();
+        
+        set.add("A");
+        set.add("D");
+        set.add("B");
+        set.add("B"); // not added, only unique
+        
+        for (String item:set) {
+            System.out.println("Item: " + item); 
+        }
+    }
+    
+}
