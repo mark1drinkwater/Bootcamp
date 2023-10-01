74f82e14f04ef3bc3898f116105fc34b55f2a9f4
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-01 07:55:22 +0200

    Create PathTest.java

---
+++ 6.Java/IgBooks/src/igbook2/lesson14/PathTest.java
@@ -0,0 +1,14 @@
+package igbook2.lesson14;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathTest {
+    public static void main(String[] args) {
+        Path p1 = Paths.get("C:\\Users\\Mark\\Documents\\Bootcamp\\6.Java");
+        System.out.format("getFileName: %s%n", p1.getFileName());
+        System.out.format("getParent: %s%n", p1.getParent());
+        System.out.format("getNameCount: %d%n", p1.getNameCount());
+        
+    }
+}
