36cf71908117f3b31eb99fede68fced62c3fb7e5
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-02 09:20:16 +0200

    Create Television.java

---
+++ 6.Java/IgBooks/src/igbook1/lesson5/generalization/Television.java
@@ -0,0 +1,14 @@
+package igbook1.lesson5.generalization;
+
+// An abstract class can be subclassed.
+public class Television extends ElectronicDevice {
+    public void turnOn() {
+//        changeChannel(1);
+//        initializeScreen();
+    }
+
+    public void turnOff() {}
+    public void changeChannel(int channel) {}
+    public void initializeScreen() {}
+
+}
