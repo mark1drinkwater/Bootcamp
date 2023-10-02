25947b9d81f1b1750cc1a7a046dea81d04b5bb2b
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-02 09:20:37 +0200

    Create ElectronicTest.java

---
+++ 6.Java/IgBooks/src/igbook1/lesson5/generalization/ElectronicTest.java
@@ -0,0 +1,11 @@
+package igbook1.lesson5.generalization;
+
+public class ElectronicTest {
+    public static void main(String[] args) {
+        // An abstract class cannot be instantiated.
+        /*
+            An abstract class is typically extended by a child class & may be used as a reference type.
+         */
+//        ElectronicDevice dev = new ElectronicDevice();
+    }
+}
