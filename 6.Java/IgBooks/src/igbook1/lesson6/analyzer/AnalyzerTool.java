ca87ee164b16a41c62ec0154efa12252f78cdab2
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-02 09:20:11 +0200

    Create AnalyzerTool.java

---
+++ 6.Java/IgBooks/src/igbook1/lesson6/analyzer/AnalyzerTool.java
@@ -0,0 +1,7 @@
+package igbook1.lesson6.analyzer;
+
+public class AnalyzerTool {
+    public boolean arrContains(String sourceStr, String searchStr) {
+        return sourceStr.contains(searchStr);
+    }
+}
