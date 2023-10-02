3a648ae20e64c193501260f06c362cd93663c2ec
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-02 09:19:53 +0200

    Update PathTest.java

--- 6.Java/IgBooks/src/igbook2/lesson14/PathTest.java
+++ 6.Java/IgBooks/src/igbook2/lesson14/PathTest.java
@@ -9,6 +9,9 @@ public class PathTest {
         System.out.format("getFileName: %s%n", p1.getFileName());
         System.out.format("getParent: %s%n", p1.getParent());
         System.out.format("getNameCount: %d%n", p1.getNameCount());
-        
+        System.out.format("getRoot: %s%n", p1.getRoot());
+        System.out.format("isAbsolute: %b%n", p1.isAbsolute());
+        System.out.format("toAbsolutePath: %s%n", p1.toAbsolutePath());
+        System.out.format("toURI: %s%n", p1.toUri());
     }
 }
