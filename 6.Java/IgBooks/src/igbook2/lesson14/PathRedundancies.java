16c6ae484cf64f84e54dfea964f2b74c0f0f7d2f
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-02 09:19:59 +0200

    Create PathRedundancies.java

---
+++ 6.Java/IgBooks/src/igbook2/lesson14/PathRedundancies.java
@@ -0,0 +1,29 @@
+package igbook2.lesson14;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathRedundancies {
+    public static void main(String[] args) {
+        String notNormalised = "\\home\\.\\clarence\\..\\peter\\foo";
+        Path p = Paths.get(notNormalised);
+        Path normalizedPath = p.normalize();
+        System.out.println(normalizedPath);
+        // Normalize does not check the file system when it cleans up a path.
+        // It is a purely syntactic operation.
+
+        // The element name closest to the root has index 0.
+        // The element farthest from the root has index count-1
+        Path p2 = normalizedPath.subpath(1, 3);
+        System.out.println("Sub path:" + p2);
+
+        // The resolve method is ued to combine paths.
+        Path p3 = Paths.get("/home/clarence/foo");
+        p3.resolve("bar"); // Returns /home/clarence/foo/bar
+
+        // The Path interface is "link aware."
+        // Detects what to do when a symbolic link is encountered
+
+
+    }
+}
