90223756f57df9a938db6440d0258b575f8df163
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-04 07:46:12 +0200

    Create StaticImport.java

---
+++ 6.Java/IgBooks/src/igbook1/lesson4/statics/StaticImport.java
@@ -0,0 +1,11 @@
+package igbook1.lesson4.statics;
+import static java.lang.Math.*;
+
+// A static import makes the static members of a class available
+// under their simple name.
+
+public class StaticImport {
+    public static void main(String[] args) {
+        double d = random();
+    }
+}
