16013fce03850d21cf8117e18adc41139744e807
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-05 06:42:29 +0200

    Create IfElseStatement.java

---
+++ 6.Java/IgBooks/src/igbook1/lesson2/IfElseStatement.java
@@ -0,0 +1,17 @@
+package igbook1.lesson2;
+
+public class IfElseStatement {
+
+    public static void main(String[] args) {
+        long a = 1;
+        long b = 1;
+        // If-else statement
+        if (a == b) {
+            System.out.println("True");
+        } else  {
+            System.out.println("False");
+        }
+
+    }
+
+}
