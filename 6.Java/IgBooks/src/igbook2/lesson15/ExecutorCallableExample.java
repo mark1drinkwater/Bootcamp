865ffba6a6dfb8cd5154bb05c0e71cb58d3dc8ab
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-06 09:16:18 +0200

    Update ExecutorCallableExample.java

--- 6.Java/IgBooks/src/igbook2/lesson15/ExecutorCallableExample.java
+++ 6.Java/IgBooks/src/igbook2/lesson15/ExecutorCallableExample.java
@@ -6,15 +6,27 @@ public class ExecutorCallableExample {
     public static void main(String[] args) {
         ExecutorService es = Executors.newFixedThreadPool(4);
         Future<String> f1 = es.submit(new ExampleCallable("one", 10));
-        Future<String> f2 = es.submit(new ExampleCallable("two", 20));
+        Future<String> f2 = es.submit(new ExampleCallable("two", 10));
 
         try {
-            es.shutdown();
+//            es.shutdown();
             // Wait 5 seconds for the tasks to complete
-            es.awaitTermination(5, TimeUnit.SECONDS);
+//            es.awaitTermination(5, TimeUnit.SECONDS);
             // Get the results of task f1 and f2
-            String result1 = f1.get();
-            System.out.println("Result of one: " + result1);
+
+            int i = 1;
+            i++;
+
+            try {
+                f1.get(1, TimeUnit.SECONDS);
+            } catch (Exception e) {
+                System.out.println("Sorry your CPU is too slow!!");
+                throw new RuntimeException();
+            }
+
+
+//            String result1 = f1.get();
+//            System.out.println("Result of one: " + result1);
             String result2 = f2.get();
             System.out.println("Result of two: " + result2);
         } catch (ExecutionException | InterruptedException e) {
