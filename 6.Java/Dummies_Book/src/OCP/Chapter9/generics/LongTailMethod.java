5056c69f471737bb9422f7bba88d874cdcec6e19
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-06 09:19:14 +0200

    Create LongTailMethod.java

---
+++ 6.Java/Dummies_Book/src/OCP/Chapter9/generics/LongTailMethod.java
@@ -0,0 +1,21 @@
+package OCP.Chapter9.generics;
+
+import java.util.List;
+
+public class LongTailMethod {
+    public void chew(String s) {
+
+    }
+
+    public void chew(Integer i) {
+
+    }
+
+    public void chewAlot(List<String> list) {
+    }
+//    public void chewAlot(List<Integer> list) {
+//    }
+
+}
+
+
