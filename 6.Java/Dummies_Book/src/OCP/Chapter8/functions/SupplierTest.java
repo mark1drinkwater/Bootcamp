3362cf43922fa53fcd79f98d5c500336b0382dd1
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-07 19:32:53 +0200

    Create SupplierTest.java

---
+++ 6.Java/Dummies_Book/src/OCP/Chapter8/functions/SupplierTest.java
@@ -0,0 +1,32 @@
+package OCP.Chapter8.functions;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.function.Supplier;
+
+public class SupplierTest {
+    public static void main(String[] args) {
+        // Supplier is often used when constructing new objects.
+        Supplier<LocalDate> s1 = LocalDate::now;
+        Supplier<LocalDate> s2 = () -> LocalDate.now();
+
+        LocalDate d1 = s1.get();
+        LocalDate d2 = s2.get();
+
+        System.out.println(d1);
+        System.out.println(d2);
+
+        Supplier<StringBuilder> sb1 = StringBuilder::new;
+        Supplier<StringBuilder> sb2 = () -> new StringBuilder();
+
+        System.out.println(sb1.get().append("Hello"));
+        System.out.println(sb2.get().append(" World"));
+
+
+        // The double <> makes it a bit more confusing
+        // But simply put we're creating an ArrayList of type String.
+        Supplier<ArrayList<String>> s3 = ArrayList::new;
+        ArrayList<String> a1 = s3.get();
+        System.out.println(a1); // []
+    }
+}
