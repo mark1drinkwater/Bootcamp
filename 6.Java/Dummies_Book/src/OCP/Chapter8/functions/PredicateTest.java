f58c5883a9cf0b6601f208214234491ff13462c5
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-07 19:32:55 +0200

    Create PredicateTest.java

---
+++ 6.Java/Dummies_Book/src/OCP/Chapter8/functions/PredicateTest.java
@@ -0,0 +1,16 @@
+package OCP.Chapter8.functions;
+
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
+public class PredicateTest {
+    public static void main(String[] args) {
+        Predicate<String> mr = String::isEmpty;
+        Predicate<String> l = s -> s.isEmpty();
+        System.out.println(mr.test(""));
+
+        BiPredicate<String, String> mr2 = String::startsWith;
+        BiPredicate<String, String> l2 = (s1, s2) -> s1.startsWith(s2);
+        System.out.println(mr2.test("Mark", "M"));
+    }
+}
