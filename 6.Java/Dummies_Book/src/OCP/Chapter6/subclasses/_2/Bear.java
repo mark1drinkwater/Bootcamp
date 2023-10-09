63f8121f951b0260a8a94575ba731d3c6926e4c4
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-09 08:26:36 +0200

    Create Bear.java

---
+++ 6.Java/Dummies_Book/src/OCP/Chapter6/subclasses/_2/Bear.java
@@ -0,0 +1,8 @@
+package OCP.Chapter6.subclasses._2;
+
+class Bird {}
+class Bear {}
+class Fish {}
+
+//protected class ClownFish{} // DOES NOT COMPILE
+//private class BlueTang {} // DOES NOT COMPILE
