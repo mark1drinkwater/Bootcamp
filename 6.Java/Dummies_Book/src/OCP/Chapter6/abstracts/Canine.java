544cfb67e330221a68e14b0c413c9a555b6bc36f
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-09 08:27:19 +0200

    Create Canine.java

---
+++ 6.Java/Dummies_Book/src/OCP/Chapter6/abstracts/Canine.java
@@ -0,0 +1,44 @@
+//package OCP.Chapter6.abstracts;
+//
+//public abstract class Canine {
+//    public abstract String getSound();
+//    public void bark() { System.out.println(getSound());
+//}
+//
+//class Wolf extends Canine {
+//
+//    @Override
+//    public String getSound() {
+//        return null;
+//    }
+//}
+//
+//class Fox extends Canine {
+//
+//    @Override
+//    public String getSound() {
+//        return null;
+//    }
+//}
+//
+//class Coyote extends Canine {
+//
+//    @Override
+//    public String getSound() {
+//        return null;
+//    }
+//}
+//
+//class CanineTest {
+//    public static void main(String[] args) {
+////        Wolf wolf = new Wolf();
+////        Fox fox = new Fox();
+////        Coyote c = new Coyote();
+//
+////        Canine c = new Canine();
+//    }
+////}
+////
+////abstract class Bear { // DOES NOT COMPILE
+////    abstract public int howl(); // DOES NOT COMPILE
+////}
