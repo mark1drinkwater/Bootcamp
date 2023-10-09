f9924443be2b658c812c6fec3c6350f151f0df80
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-09 08:26:43 +0200

    Create Jaguar.java

---
+++ 6.Java/Dummies_Book/src/OCP/Chapter6/subclasses/_1/Jaguar.java
@@ -0,0 +1,10 @@
+package OCP.Chapter6.subclasses._1;
+
+public class Jaguar extends BigCat {
+    public Jaguar() {
+        size = 10.2;
+    }
+    public void printDetails() {
+        System.out.print(size);
+    }
+}
