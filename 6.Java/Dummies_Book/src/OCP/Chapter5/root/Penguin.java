6cd407b40979db5cae72d48a96df69ae839a99cb
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-10 07:17:23 +0200

    Create Penguin.java

---
+++ 6.Java/Dummies_Book/src/OCP/Chapter5/root/Penguin.java
@@ -0,0 +1,21 @@
+package root;
+
+public class Penguin {
+    String name;
+    static String nameOfTallestPenguin;
+
+    public static void main(String[] args) {
+        var p1 = new Penguin();
+        p1.name = "Lilly";
+        p1.nameOfTallestPenguin = "Lilly";
+
+        var p2 = new Penguin();
+        p2.name = "Willy";
+        p2.nameOfTallestPenguin = "Willy";
+
+        System.out.println(p1.name); // Lilly
+        System.out.println(p1.nameOfTallestPenguin); // Willy
+        System.out.println(p2.name); // Willy
+        System.out.println(p2.nameOfTallestPenguin); // Willy
+    }
+}
