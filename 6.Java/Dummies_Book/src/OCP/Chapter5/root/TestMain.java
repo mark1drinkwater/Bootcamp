6883e52801c3841146b1a8f0d280fd6683a15c98
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-10 07:16:53 +0200

    Create TestMain.java

---
+++ 6.Java/Dummies_Book/src/OCP/Chapter5/root/TestMain.java
@@ -0,0 +1,9 @@
+package root;
+
+public class TestMain {
+    public static void main(String[] args) {
+        String[] hello = {"helo"};
+        Penguin.main(hello);
+        // Penguin.main(new String[4]);
+    }
+}
