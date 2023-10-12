a1bb6a2a4c8fe60b03087a7b70c494e60d853d25
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-12 08:49:24 +0200

    Create A4_FinalClasses.java

---
+++ 6.Java/OCP_Preparation/src/ch6/categorised/A4_FinalClasses.java
@@ -0,0 +1,8 @@
+package ch6.categorised;
+
+public class A4_FinalClasses {
+}
+
+class Mammal {}
+final class Rhinoceros extends Mammal {}
+//public class Clara extends Rhinoceros {} // Does Not Compile
