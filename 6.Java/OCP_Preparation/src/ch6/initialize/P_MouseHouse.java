7333ca2b99fc2952f05603a675bf46dd9d5c7066
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-12 08:48:39 +0200

    Create P_MouseHouse.java

---
+++ 6.Java/OCP_Preparation/src/ch6/initialize/P_MouseHouse.java
@@ -0,0 +1,24 @@
+package OCP.Chapter6.initialize;
+
+public class P_MouseHouse {
+    private final int volume;
+    private final String type;
+    private final String name = "The Mouse House"; // Declaration assignment
+
+    {
+        volume = 10; // Instance initializer assignment
+    }
+    public P_MouseHouse(String type) {
+        this.type = type;
+    }
+    public P_MouseHouse() {
+//        Already initialised it in the initialization block.
+//        this.volume = 2; // DOES NOT COMPILE
+
+        // Three different fixes:
+        // Fix 1: Initialise it in the constructor.
+        type = "Mansion";
+        // 2nd fix would be it initialise it in the same line that it is declared.
+        // 3rd fix would be to initialise it in the initialization block.
+    }
+}
