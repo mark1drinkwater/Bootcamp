a7cd4fe61250772389e62d59c49771f61ab999e4
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-12 08:49:02 +0200

    Create L_Gopher.java

---
+++ 6.Java/OCP_Preparation/src/ch6/categorised/constructors/L_Gopher.java
@@ -0,0 +1,19 @@
+package ch6.categorised.constructors;
+
+public class L_Gopher {
+}
+
+class Gopher {
+    public Gopher(int dugHoles) {
+//        this(5); // DOES NOT COMPILE
+    }
+}
+
+class Gopher2 {
+//    public Gopher() {
+//        this(5); // DOES NOT COMPILE
+//    }
+//    public Gopher(int dugHoles) {
+//        this(); // DOES NOT COMPILE
+//    }
+}
