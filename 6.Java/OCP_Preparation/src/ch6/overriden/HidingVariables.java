229f4ade0faa85ad1ae3f9375df211407e2cf528
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-12 08:48:30 +0200

    Create HidingVariables.java

---
+++ 6.Java/OCP_Preparation/src/ch6/overriden/HidingVariables.java
@@ -0,0 +1,17 @@
+package ch6.overriden;
+
+public class HidingVariables {}
+
+class Carnivore {
+    protected boolean hasFur = false;
+}
+class Meerkat extends Carnivore {
+    protected boolean hasFur = true;
+
+    public static void main(String[] args) {
+        Meerkat m = new Meerkat();
+        Carnivore c = m;
+        System.out.println(m.hasFur); // true
+        System.out.println(c.hasFur); // false
+    }
+}
