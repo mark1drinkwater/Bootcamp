a62b0030ce65b3fa61b4fc4434a7d5345cc31847
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-12 08:48:48 +0200

    Create G_Beetle.java

---
+++ 6.Java/OCP_Preparation/src/ch6/categorised/G_Beetle.java
@@ -0,0 +1,30 @@
+package ch6.categorised;
+
+class Insect {
+    protected int numberOfLegs = 4;
+    private String species = "Fly";
+    String label = "buggy";
+}
+
+class Beetle extends Insect {
+    protected int numberOfLegs = 6;
+    short age = 3;
+
+    public void printData() {
+        System.out.println(this.label);
+        System.out.println(super.label);
+        System.out.println(this.age);
+//        System.out.println(super.age); Does not compile
+        System.out.println(numberOfLegs);
+//        System.out.println(super.species); Does not compile
+            /*
+            Since both super and this have access to the inherited members.
+            It's better to just use this.
+            Only use super when you have a naming conflict.
+             */
+    }
+
+    public static void main(String[] n) {
+        new Beetle().printData();
+    }
+}
