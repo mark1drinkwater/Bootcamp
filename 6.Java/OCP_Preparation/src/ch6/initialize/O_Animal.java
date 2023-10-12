e4e9e4dd5411a0b14cb24c2c1d09e3b79f1edbad
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-12 08:48:42 +0200

    Create O_Animal.java

---
+++ 6.Java/OCP_Preparation/src/ch6/initialize/O_Animal.java
@@ -0,0 +1,18 @@
+package ch6.initialize;
+
+class Animal {
+    static { System.out.print("A"); }
+}
+
+class Hippo extends Animal {
+    public static void main(String[] grass) {
+        System.out.print("C");
+        new Hippo();
+        new Hippo();
+        new Hippo();
+    }
+    static { System.out.print("B"); }
+    /*
+    First the parent class is initialized then the child class.
+     */
+}
