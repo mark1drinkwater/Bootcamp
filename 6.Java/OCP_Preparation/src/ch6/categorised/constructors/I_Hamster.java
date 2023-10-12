fbeab9f272b1fbd59c70f2e50fd3306e456ab3d0
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-12 08:49:10 +0200

    Create I_Hamster.java

---
+++ 6.Java/OCP_Preparation/src/ch6/categorised/constructors/I_Hamster.java
@@ -0,0 +1,26 @@
+package OCP.Chapter6.subclasses.constructors;
+
+public class I_Hamster {
+    private String color;
+    private int weight;
+    public I_Hamster(int weight, String color) { // First constructor
+        this.weight = weight;
+        this.color = color;
+    }
+    public I_Hamster(int weight) { // Second constructor
+        this(weight, "brown");
+        // this or super must always be the first statement, always.
+    }
+
+    public static void main(String[] args) {
+        var h1 = new OCP.Chapter6.subclasses.constructors.I_Hamster(12);
+        System.out.println(h1);
+        var h2 = new OCP.Chapter6.subclasses.constructors.I_Hamster(12, "white");
+        System.out.println(h2);
+    }
+
+    @Override
+    public String toString() {
+        return "Weight:%d Color:%s".formatted(this.weight, this.color);
+    }
+}
