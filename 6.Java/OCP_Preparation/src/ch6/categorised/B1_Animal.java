f6169e71d6ef40e68e90ca7bc3f619bb8215fba2
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-12 08:49:21 +0200

    Create B1_Animal.java

---
+++ 6.Java/OCP_Preparation/src/ch6/categorised/B1_Animal.java
@@ -0,0 +1,12 @@
+package ch6.categorised;
+
+public class B1_Animal {
+    private int age;
+    protected String name;
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int newAge) {
+        age = newAge;
+    }
+}
