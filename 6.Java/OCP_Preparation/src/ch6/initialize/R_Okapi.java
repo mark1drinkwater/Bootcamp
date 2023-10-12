e846416fb81d04f3dd027f23d3226b5d3aa33184
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-12 08:48:35 +0200

    Create R_Okapi.java

---
+++ 6.Java/OCP_Preparation/src/ch6/initialize/R_Okapi.java
@@ -0,0 +1,45 @@
+package OCP.Chapter6.initialize;
+
+class GiraffeFamily {
+    static {
+        System.out.print("A");
+    }
+
+    {
+        System.out.print("B");
+    }
+
+    public GiraffeFamily(String name) {
+        this(1);
+        System.out.print("C");
+    }
+
+    public GiraffeFamily() {
+        System.out.print("D");
+    }
+
+    public GiraffeFamily(int stripes) {
+        System.out.print("E");
+    }
+}
+
+class Okapi extends GiraffeFamily {
+    static {
+        System.out.print("F");
+    }
+
+    public Okapi(int stripes) {
+        super("sugar");
+        System.out.print("G");
+    }
+
+    {
+        System.out.print("H");
+    }
+
+    public static void main(String[] grass) {
+        new Okapi(1);
+        System.out.println();
+        new Okapi(2);
+    }
+}
