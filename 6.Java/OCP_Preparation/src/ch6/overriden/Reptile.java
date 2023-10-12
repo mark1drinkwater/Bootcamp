c3c71e971a7ad0b7753d73f77c4d0753710233b5
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-12 08:48:25 +0200

    Create Reptile.java

---
+++ 6.Java/OCP_Preparation/src/ch6/overriden/Reptile.java
@@ -0,0 +1,20 @@
+package OCP.Chapter6.overriden;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Reptile {
+    protected void sleep() throws IOException {}
+    protected void hide() {}
+    protected void exitShell() throws FileNotFoundException {}
+}
+
+//class GalapagosTortoise extends Reptile {
+//    // FileNotFoundException is a sub-type of IOException
+//    public void sleep() throws FileNotFoundException {}
+//    // The parent method does not throw a exception
+//    public void hide() throws FileNotFoundException {} // DOES NOT COMPILE
+//    // IOException is not a sub-type exception of FileNotFoundException.
+//    // FileNotFoundException is a subtype of IOException
+//    public void exitShell() throws IOException {} // DOES NOT COMPILE
+//}
