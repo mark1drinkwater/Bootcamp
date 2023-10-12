7fd2dc0af50fc304fe7c64576c938c4da3b2f9e5
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-12 08:48:33 +0200

    Create FinalMethods.java

---
+++ 6.Java/OCP_Preparation/src/ch6/overriden/FinalMethods.java
@@ -0,0 +1,16 @@
+package ch6.overriden;
+
+public class FinalMethods {}
+
+class Bird {
+    public final boolean hasFeathers() {
+        return true;
+    }
+    public final static void flyAway() {}
+}
+class Penguin extends Bird {
+//    public final boolean hasFeathers() { // DOES NOT COMPILE
+//        return false;
+//    }
+//    public final static void flyAway() {} // DOES NOT COMPILE
+}
