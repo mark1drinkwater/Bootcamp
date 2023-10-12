3e056ab6a71616baaf033216ee4ec1140b278e52
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-12 08:49:29 +0200

    Create A2_Jaguar.java

---
+++ 6.Java/OCP_Preparation/src/ch6/categorised/A2_Jaguar.java
@@ -0,0 +1,10 @@
+package ch6.categorised;
+
+public class A2_Jaguar extends A1_BigCat {
+    public A2_Jaguar() {
+        size = 10.2;
+    }
+    public void printDetails() {
+        System.out.print(size);
+    }
+}
