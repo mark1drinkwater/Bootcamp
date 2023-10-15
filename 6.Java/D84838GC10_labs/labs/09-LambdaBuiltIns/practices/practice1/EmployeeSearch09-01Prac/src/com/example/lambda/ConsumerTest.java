92e823e78fe68eff2224b31a0bc5eeba420bd22f
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-15 07:48:04 +0200

    Oracle IG Book Ch9 Practice 1

---
+++ 6.Java/D84838GC10_labs/labs/09-LambdaBuiltIns/practices/practice1/EmployeeSearch09-01Prac/src/com/example/lambda/ConsumerTest.java
@@ -0,0 +1,26 @@
+package com.example.lambda;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ *
+ * @author oracle
+ */
+public class ConsumerTest {
+  
+  public static void main(String[] args) {
+    
+    List<Employee> eList = Employee.createShortList();
+    Employee first = eList.get(0);
+    
+    // Write your consumer lambda here
+    Consumer<Employee> eCons = 
+            t -> System.out.println("Name: " + t.getSurName() + 
+                    " Role: " + t.getRole() + "   Salary: " + t.getSalary() );
+    
+    System.out.println("=== First Salary");
+    // Call your consumer here
+    eCons.accept(first);
+  }
+}
