c6387aa2dff72eaa4a45e47054c56951c4ee0e42
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-18 07:15:57 +0200

    Reduction lambda expression example

---
+++ 6.Java/D84838GC10_labs/labs/17-ParallelStreams/examples/EmployeeSearch17/src/com/example/lambda/A08Reduction.java
@@ -0,0 +1,32 @@
+package com.example.lambda;
+
+import java.util.stream.IntStream;
+
+/**
+ *
+ * @author oracle
+ */
+public class A08Reduction {
+    
+    public static void main(String[] args) {
+        
+        // Reduction takes a sequence of input elements and combines them
+        // into a single summary result by repeated application of a combining operation.
+        int r1 = IntStream.rangeClosed(1, 5).parallel()
+            .reduce(0, (a, b) -> a + b);
+        
+        System.out.println("Result: " + r1);
+        
+        int r2 = IntStream.rangeClosed(1, 5).parallel()
+            .reduce(0, (sum, element) -> sum + element);
+        
+        
+        System.out.println("Result: " + r2);
+        
+        int r3 = IntStream.rangeClosed(1, 8).parallel().reduce(0, (sum, element) -> sum + element);
+        
+        System.out.println("Result: " + r3);
+        
+    }
+    
+}
