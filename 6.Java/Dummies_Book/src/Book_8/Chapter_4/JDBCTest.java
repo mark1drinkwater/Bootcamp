2872bd8dd9815f011d65cd63e0bac1fe19d4932a
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-21 06:48:50 +0200

    Update JDBCTest.java

--- 6.Java/Dummies_Book/src/Book_8/Chapter_4/JDBCTest.java
+++ 6.Java/Dummies_Book/src/Book_8/Chapter_4/JDBCTest.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class JDBCTest {
 
     public static void main(String[] args) {
-        Connection con = getconnection();
+            Connection con = getconnection();
     }
 
     private static Connection getconnection()
