97c84b15a6014f166eedd00dea33918c21edd6f2
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-24 17:06:46 +0200

    imported chapter 1,3,4 spring code

--- 7.Spring_Framework/AllTacoSourceCode/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ 7.Spring_Framework/AllTacoSourceCode/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package tacos;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TacoCloudApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
