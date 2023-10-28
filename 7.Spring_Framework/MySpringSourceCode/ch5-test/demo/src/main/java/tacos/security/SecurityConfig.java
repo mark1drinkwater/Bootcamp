f648ba6237b51cd1349e0b1d9f8f8d0619988c5b
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-28 09:04:19 +0200

    Ch5 test code

---
+++ 7.Spring_Framework/MySpringSourceCode/ch5-test/demo/src/main/java/tacos/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package tacos.security;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import tacos.data.UserRepository;
+
+@Configuration
+public class SecurityConfig {
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+		List<UserDetails> usersList = new ArrayList<>();
+		usersList.add(
+				new User("buzz", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+		usersList.add(
+				new User("woody", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+		return new InMemoryUserDetailsManager(usersList);
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService(UserRepository userRepo) {
+	 return username -> {
+	 tacos.User user = userRepo.findByUsername(username);
+	 if (user != null) return user;
+	 throw new UsernameNotFoundException("User '" + username + "' not found");
+	 };
+	}
+}
+
+
