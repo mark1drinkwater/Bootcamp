05d0264d254370ebfc95c9861e5151685ebd9888
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-11-02 21:07:39 +0200

    C# data types and casting

--- CSharp/CsharpProjects/TestProject/Program.cs
+++ CSharp/CsharpProjects/TestProject/Program.cs
@@ -1,4 +1,7 @@
-﻿// int[] inventory = { 200, 450, 700, 175, 250 };
+﻿using System.Globalization;
+CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+// int[] inventory = { 200, 450, 700, 175, 250 };
 // int sum = 0;
 
 // int bin = 0;
@@ -106,22 +109,73 @@
 // Console.WriteLine(new_message);
 // Console.WriteLine($"'o' appears {letterCount} times.");
 
-Console.WriteLine("Signed integral types:");
+// Console.WriteLine("Signed integral types:");
 
-Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
-Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
-Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
-Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+// Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+// Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+// Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+// Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
 
 
-Console.WriteLine("");
-Console.WriteLine("Unsigned integral types:");
-Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
-Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
-Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
-Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+// Console.WriteLine("");
+// Console.WriteLine("Unsigned integral types:");
+// Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+// Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+// Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+// Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
 
-int[] data = new int[3];
+// int[] data = new int[3];
 
-string shortenedString = "Hello World!";
-Console.WriteLine(shortenedString);
+// string shortenedString = "Hello World!";
+// Console.WriteLine(shortenedString);
+
+// int myInt = 3;
+// Console.WriteLine($"int: {myInt}");
+
+// decimal myDecimal = myInt;
+// Console.WriteLine($"decimal: {myDecimal}");
+
+// decimal myDecimal = 3.14m;
+// Console.WriteLine($"decimal: {myDecimal}");
+
+// int myInt = (int)myDecimal;
+// Console.WriteLine($"int: {myInt}");
+
+// decimal myDecimal = 1.23456789m;
+// float myFloat = (float)myDecimal; // narrowing conversion
+
+// Console.WriteLine($"Decimal: {myDecimal}");
+// Console.WriteLine($"Float  : {myFloat}");
+
+// string first = "5";
+// string second = "7";
+// int sum = int.Parse(first) + int.Parse(second);
+// Console.WriteLine(sum);
+
+// string value1 = "5";
+// string value2 = "7";
+// int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+// Console.WriteLine(result);
+
+// int value = (int)1.5m; // casting truncates
+// Console.WriteLine(value);
+
+// int value2 = Convert.ToInt32(1.5m); // converting rounds up
+// Console.WriteLine(value2);
+// 
+// string name = "Bob";
+// Console.WriteLine(int.Parse(name)); // If you want to avoid format exception, use TryParse()
+
+string value = "bad";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+    Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+    Console.WriteLine("Unable to report the measurement.");
+}
+
+if (result > 0)
+    Console.WriteLine($"Measurement (w/ offset): {50 + result}");
