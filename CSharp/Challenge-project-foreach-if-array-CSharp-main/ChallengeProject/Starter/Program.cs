b5d09dace296680eee9d935f51696f1576494929
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-11-02 10:13:44 +0200

    C# do-while loops

--- CSharp/Challenge-project-foreach-if-array-CSharp-main/ChallengeProject/Starter/Program.cs
+++ CSharp/Challenge-project-foreach-if-array-CSharp-main/ChallengeProject/Starter/Program.cs
@@ -389,22 +389,53 @@ This C# console application is designed to:
 
 // foreach (var name in names) Console.WriteLine(name);
 
-for (int i = 1; i <= 100; i++)
-{
-    if ( (i % 3 == 0) && (i % 5 == 0) )
-    {
-        Console.WriteLine(i + " - FizzBuzz ");
-    }
-    else if (i % 3 == 0)
-    {
-        Console.WriteLine(i + " - Fizz");
-    }
-    else if (i % 5 == 0)
-    {
-        Console.WriteLine(i + " - Buzz");
-    }
-    else
-    {
-        Console.WriteLine(i);
-    }
-}
+// for (int i = 1; i <= 100; i++)
+// {
+//     if ( (i % 3 == 0) && (i % 5 == 0) )
+//     {
+//         Console.WriteLine(i + " - FizzBuzz ");
+//     }
+//     else if (i % 3 == 0)
+//     {
+//         Console.WriteLine(i + " - Fizz");
+//     }
+//     else if (i % 5 == 0)
+//     {
+//         Console.WriteLine(i + " - Buzz");
+//     }
+//     else
+//     {
+//         Console.WriteLine(i);
+//     }
+// }
+
+// Random random = new Random();
+// int current = 0;
+
+// do
+// {
+//     current = random.Next(1, 11);
+//     Console.WriteLine(current);
+// } while (current != 7);
+
+// Random random = new Random();
+// int current = random.Next(1, 11);
+
+// while (current >= 3)
+// {
+//     Console.WriteLine(current);
+//     current = random.Next(1, 11);
+// }
+// Console.WriteLine($"Last number: {current}");
+
+// Random random = new Random();
+// int current = random.Next(1, 11);
+
+// do
+// {
+//     current = random.Next(1, 11);
+//     if (current >= 8)
+//         continue;
+    
+//     Console.WriteLine(current);
+// } while (current != 7);
