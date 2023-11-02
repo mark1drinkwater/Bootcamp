37e3a41e34306ba79c9ffda2c72668f0a49f30a1
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-11-02 08:08:33 +0200

    Completed Decision logic challenge

--- CSharp/CsharpProjects/TestProject/Program.cs
+++ CSharp/CsharpProjects/TestProject/Program.cs
@@ -1,6 +1,107 @@
-﻿string[] names = { "Rowena", "Robin", "Bao" };
+﻿// int[] inventory = { 200, 450, 700, 175, 250 };
+// int sum = 0;
 
-foreach (String name in names)
-{
-    Console.WriteLine(name);
+// int bin = 0;
+// foreach (int items in inventory)
+// {
+//     sum += items;
+//     bin++;
+//     Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+// }
+
+// Console.WriteLine($"We have {sum} items in inventory.");
+
+// Fraudulent order challenge
+// String[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+// foreach (String orderID in orderIDs)
+// {
+//     if (orderID.StartsWith("B"))
+//     {
+//         Console.WriteLine(orderID);
+//     }
+// }
+
+// Random random = new Random();
+// string[] orderIDs = new string[5];
+// // Loop through each blank orderID
+// for (int i = 0; i < orderIDs.Length; i++)
+// {
+//     // Get a random value that equates to ASCII letters A through E
+//     int prefixValue = random.Next(65, 70);
+//     // Convert the random value into a char, then a string
+//     string prefix = Convert.ToChar(prefixValue).ToString();
+//     // Create a random number, pad with zeroes
+//     string suffix = random.Next(1, 1000).ToString("000");
+//     // Combine the prefix and suffix together, then assign to current OrderID
+//     orderIDs[i] = prefix + suffix;
+// }
+// // Print out each orderID
+// foreach (var orderID in orderIDs)
+// {
+//     Console.WriteLine(orderID);
+// }
+
+/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+*/
+
+
+// Random random = new Random();
+// string[] orderIDs = new string[5];
+
+// for (int i = 0; i < orderIDs.Length; i++)
+// {
+//     int prefixValue = random.Next(65, 70);
+//     string prefix = Convert.ToChar(prefixValue).ToString();
+//     string suffix = random.Next(1, 1000).ToString("000");
+
+//     orderIDs[i] = prefix + suffix;
+// }
+
+// foreach (var orderID in orderIDs)
+// {
+//     Console.WriteLine(orderID);
+// }
+
+// Example 1:
+
+// Console
+// .
+// WriteLine
+// (
+// "Hello Example 1!"
+// )
+// ;
+
+// // Example 2:
+// string firstWord="Hello";string lastWord="Example 2";Console.WriteLine(firstWord+" "+lastWord+"!");
+
+/*
+    Reverses a string message.
+    Then counts the number of times 'o' appears.
+    Prints the output to the console.
+*/
+
+string str = "The quick brown fox jumps over the lazy dog.";
+
+char[] charMessage = str.ToCharArray();
+Array.Reverse(charMessage);
+
+int letterCount = 0;
+
+foreach (char i in charMessage) 
+{ 
+    if (i == 'o') 
+    { 
+        letterCount++; 
+    } 
 }
+
+string new_message = new String(charMessage);
+
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {letterCount} times.");
---
+++ CSharp/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+// initialize variables - graded assignments 
+int examAssignments = 5;
+
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
+int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
+int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
+int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 };    
+
+// Student names
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
+
+int[] studentScores;
+
+Console.WriteLine("Student\t\tGrade\n");
+foreach (string name in studentNames)
+{
+    if (name == "Sophia")
+        studentScores = sophiaScores;
+    else if (name == "Andrew")
+        studentScores = andrewScores;
+    else if (name == "Emma")
+        studentScores = emmaScores;
+    else if (name == "Logan")
+        studentScores = loganScores;
+    else if (name == "Becky")
+        studentScores = beckyScores;
+    else if (name == "Chris")
+        studentScores = chrisScores;
+    else if (name == "Eric")
+        studentScores = ericScores;
+    else if (name == "Gregor")
+        studentScores = gregorScores;
+    else
+        continue;    
+
+    int sum = 0;
+    int gradedAssignments = 0;
+    foreach (int score in studentScores)
+    {
+        gradedAssignments++;
+        if (gradedAssignments > examAssignments)
+        {
+            sum += score / 10;
+        }
+        else 
+        {
+            sum += score;
+        }
+    }
+
+    decimal avg = (decimal)sum / examAssignments;
+
+    string currentGrade;
+
+    if (avg >= 97)
+        currentGrade = "A+";
+    else if (avg >= 93)
+        currentGrade = "A";
+    else if (avg >= 90)
+        currentGrade = "A-";
+    else if (avg >= 87)
+        currentGrade = "B+";
+    else if (avg >= 83)
+        currentGrade = "B";
+    else if (avg >= 80)
+        currentGrade = "B-";
+    else if (avg >= 77)
+        currentGrade = "C+";
+    else if (avg >= 73)
+        currentGrade = "C";
+    else if (avg >= 70)
+        currentGrade = "C-";
+    else if (avg >= 67)
+        currentGrade = "D+";
+    else if (avg >= 63)
+        currentGrade = "D";
+    else if (avg >= 60)
+        currentGrade = "D-";
+    else
+        currentGrade = "F";
+
+    Console.WriteLine($"{name}:\t\t{avg}\t{currentGrade}");    
+}
+
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
