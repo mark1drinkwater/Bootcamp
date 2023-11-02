a393c37fbed8598caec75b83c3964541a5c34901
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-11-02 10:05:45 +0200

    FizzBuzz challenge

--- CSharp/Challenge-project-foreach-if-array-CSharp-main/ChallengeProject/Starter/Program.cs
+++ CSharp/Challenge-project-foreach-if-array-CSharp-main/ChallengeProject/Starter/Program.cs
@@ -389,3 +389,22 @@ This C# console application is designed to:
 
 // foreach (var name in names) Console.WriteLine(name);
 
+for (int i = 1; i <= 100; i++)
+{
+    if ( (i % 3 == 0) && (i % 5 == 0) )
+    {
+        Console.WriteLine(i + " - FizzBuzz ");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine(i + " - Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine(i + " - Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
