4fee8fddee38d5c99802bf02d85ad4f8f7aaadf7
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-11-03 18:17:49 +0200

    C# array sort

---
+++ CSharp/CsharpProjects/Arrays-parse-sort-orders-challenge/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] orders = orderStream.Split(',');
+Array.Sort(orders);
+
+foreach (string order in orders)
+{
+    Console.Write(order);
+    if (order.Length != 4)
+    {
+        Console.Write("\t\t - Error");
+    }
+    Console.WriteLine();
+}
