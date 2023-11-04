7b3379e04c4a63f243c225b43f4ec73fac5c8451
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-11-04 14:49:52 +0200

    C# fortune exercise

---
+++ CSharp/Methods_Branch/fortune-exercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+Random random = new Random();
+
+string[] text = { "You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to" };
+string[] good = { "look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!" };
+string[] bad = { "fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life." };
+string[] neutral = { "appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature." };
+
+TellFortune();
+TellFortune();
+
+void TellFortune()
+{
+    Console.WriteLine("A fortune teller whispers the following words:");
+    int luck = random.Next(100);
+    string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
+    for (int i = 0; i < 4; i++)
+    {
+        Console.Write($"{text[i]} {fortune[i]} ");
+    }
+    Console.WriteLine();
+}
+
