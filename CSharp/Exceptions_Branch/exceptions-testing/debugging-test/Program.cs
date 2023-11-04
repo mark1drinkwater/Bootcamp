c441bed1616c28f568d305571e3856063bb0f662
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-11-04 19:03:55 +0200

    New C# exceptions project

---
+++ CSharp/Exceptions_Branch/exceptions-testing/debugging-test/Program.cs
@@ -0,0 +1,8 @@
+﻿//TryCatchChallenge tryCatchChallenge = new TryCatchChallenge();
+//Class1 class1 = new Class1();
+
+//Class2 class2 = new Class2();
+
+//CreateThrowExceptions createThrowExceptions = new();
+
+CreateThrowExceptionsExercise ctee = new();
