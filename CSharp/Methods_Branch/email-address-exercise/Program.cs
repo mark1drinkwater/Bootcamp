c8286ef64e229a20a07524fc4299c9cdb1e88777
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-11-04 14:49:42 +0200

    C# email address exercise

---
+++ CSharp/Methods_Branch/email-address-exercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+CultureInfo.CurrentCulture = new CultureInfo("en-US");
+string[,] corporate = 
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++) 
+{
+    // display internal email addresses
+    DisplayEmail(corporate[i,0], corporate[i,1]);
+}
+
+for (int i = 0; i < external.GetLength(0); i++) 
+{
+    // display external email addresses
+    DisplayEmail(external[i,0], external[i,1], externalDomain);
+}
+
+void DisplayEmail(string firstName, string lastName, string domainName = "contoso.com")
+{
+    string email = firstName.Substring(0,2).ToLower() + lastName.ToLower() + "@" + domainName.ToLower();
+    Console.WriteLine(email);
+}
