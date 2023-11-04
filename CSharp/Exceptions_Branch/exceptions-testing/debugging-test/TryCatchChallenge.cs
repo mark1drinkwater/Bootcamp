7f82c1184d252852706dd4da7f95dcc8e6c8951c
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-11-04 19:04:39 +0200

    C# exception challenge

---
+++ CSharp/Exceptions_Branch/exceptions-testing/debugging-test/TryCatchChallenge.cs
@@ -0,0 +1,48 @@
+﻿public class TryCatchChallenge
+{
+    public TryCatchChallenge()
+    {
+        try
+        {
+            Process1();
+        }
+        catch
+        {
+            Console.WriteLine("An exception has occurred");
+        }
+
+        Console.WriteLine("Exit program");
+
+        static void Process1()
+        {
+            try
+            {
+                WriteMessage();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception caught in Process1: {ex.Message}");
+            }
+        }
+
+        static void WriteMessage()
+        {
+            double float1 = 3000.0;
+            double float2 = 0.0;
+            int number1 = 3000;
+            int number2 = 0;
+            byte smallNumber;
+
+            Console.WriteLine(float1 / float2);
+            //Console.WriteLine(number1 / number2);
+
+            // Without checked this will not generate an exception if overflow occurs.
+            checked
+            {
+                smallNumber = (byte)number1;
+            }
+
+        }
+
+    }
+}
